eb7cbe67158924a0de114664ce5251fcad418b12
Liam Girdwood <liam@localhost.localdomain>
2007-03-04 16:58:40 +0000

    Add Neo1973 ASoC support.
    Signed-off-by: Graeme Gregory <gg@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lg@opensource.wolfsonmicro.com>

---
+++ sound/soc/s3c24xx/lm4857.h
@@ -0,0 +1,15 @@
+#ifndef LM4857_H_
+#define LM4857_H_
+
+/* The register offsets in the cache array */
+#define LM4857_MVOL 0
+#define LM4857_LVOL 1
+#define LM4857_RVOL 2
+#define LM4857_CTRL 3
+
+/* the shifts required to set these bits */
+#define LM4857_3D 5
+#define LM4857_WAKEUP 5
+#define LM4857_EPGAIN 4
+
+#endif /*LM4857_H_*/
