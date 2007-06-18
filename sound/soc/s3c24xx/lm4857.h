84f16eae9d4d5106c7bca040156af6e905e792a3
Graeme Gregory <gg@gg-desktop.wolfsonmicro.main>
2007-06-18 14:06:16 +0100

    lm4857.h add a copyright header.
    Signed-off-by: Graeme Gregory <gg@opensource.wolfsonmicro.com>

--- sound/soc/s3c24xx/lm4857.h
+++ sound/soc/s3c24xx/lm4857.h
@@ -1,3 +1,19 @@
+/*
+ * lm4857.h  --  ALSA Soc Audio Layer
+ *
+ * Copyright 2007 Wolfson Microelectronics PLC.
+ * Author: Graeme Gregory
+ *         graeme.gregory@wolfsonmicro.com or linux@wolfsonmicro.com
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
+ *
+ *  Revision history
+ *    18th Jun 2007   Initial version.
+ */
+
 #ifndef LM4857_H_
 #define LM4857_H_
 
