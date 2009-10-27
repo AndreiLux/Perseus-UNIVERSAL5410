5f2e9fd0725ec7ebab543fd632facc468aa61190
Scott James Remnant <scott@ubuntu.com>
2009-10-27 10:05:32 +0000

    UBUNTU: SAUCE: (no-up) trace: add trace events for open(), exec() and uselib()
    BugLink: http://bugs.launchpad.net/bugs/462111
    
    This patch uses TRACE_EVENT to add tracepoints for the open(),
    exec() and uselib() syscalls so that ureadahead can cheaply trace
    the boot sequence to determine what to read to speed up the next.
    
    It's not upstream because it will need to be rebased onto the syscall
    trace events whenever that gets merged, and is a stop-gap.
    
    Signed-off-by: Scott James Remnant <scott@ubuntu.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Andy Whitcroft <andy.whitcroft@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- fs/exec.c
+++ fs/exec.c
@@ -56,6 +56,8 @@
 #include <linux/oom.h>
 #include <linux/compat.h>
 
+#include <trace/events/fs.h>
+
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
@@ -780,6 +782,8 @@ struct file *open_exec(const char *name)
 		goto exit;
 
 	fsnotify_open(file);
+
+	trace_open_exec(name);
 
 	err = deny_write_access(file);
 	if (err)
