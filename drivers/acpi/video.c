b98a76cb615adbb57a9c37f95c4b960dbdf5a011
Andy Whitcroft <apw@canonical.com>
2009-11-09 17:30:29 +0000

    UBUNTU: SAUCE: (no-up) set /proc/acpi/video/*/DOS to 4 by default
    BugLink: http://bugs.launchpad.net/bugs/458982
    
    acpi_video_bus_start_devices() in drivers/acpi/video.c sets the default
    ACPI DOS value to 0, which lets the OS handle toggling of display output
    but still leaves the BIOS handling brightness levels automatically when
    connecting/disconnecting AC.
    
    We want the OS to handle both where possible, so a better default would
    be to set this to 4.
    
    This likely will regress systems using proprietary video drivers which
    will need to change this setting back using the sysfs interfaces.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- drivers/acpi/video.c
+++ drivers/acpi/video.c
@@ -1425,7 +1425,7 @@ static int acpi_video_bus_put_devices(struct acpi_video_bus *video)
 
 static int acpi_video_bus_start_devices(struct acpi_video_bus *video)
 {
-	return acpi_video_bus_DOS(video, 0, 0);
+	return acpi_video_bus_DOS(video, 0, 1);
 }
 
 static int acpi_video_bus_stop_devices(struct acpi_video_bus *video)
