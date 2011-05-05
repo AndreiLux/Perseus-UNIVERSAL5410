85df2d28c5e10ba59b6dd215508c7d2eda0b0e69
Elly Jones <ellyjones@chromium.org>
2011-05-05 18:16:51 -0400

    CHROMIUM: Add Dell Gobi 3000 vid:pid.
    Note that the QDL vid:pid is not yet tested, since I don't have a physical
    sample.
    
    BUG=chromium-os:14957
    TEST=Adhoc
    Built. No hardware yet, so no test yet.
    
    Change-Id: I38f29d0cf782c570c76c777cf6100087f5d43cb4
    Signed-off-by: Elly Jones <ellyjones@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/380
    Reviewed-by: Jason Glasgow <jglasgow@chromium.org>
    
    [v3.4: dropped changes to qcserial.c]

--- drivers/net/usb/gobi/qcusbnet.c
+++ drivers/net/usb/gobi/qcusbnet.c
@@ -578,6 +578,7 @@ static const struct usb_device_id qc_vidpids[] = {
 
 	MKVIDPID(0x05c6, 0x920d),	/* Qualcomm Gobi 3000 */
 	MKVIDPID(0x1410, 0xa021),	/* Novatel Gobi 3000 */
+	MKVIDPID(0x413c, 0x8194),	/* Dell Gobi 3000 */
 	{ }
 };
 
