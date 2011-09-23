976f6024272aa15174485c762dfe411656c106a9
Sourav Poddar <sourav.poddar@ti.com>
2011-09-23 18:51:34 +0530

    omap5: evm: Add smsc keypad board file data
    Add smsc keymap data to omap5 board file. Also populate
    i2c5 with smsc slave address and gpio(used as an irq).
    
    Signed-off-by: Sourav Poddar <sourav.poddar@ti.com>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -25,7 +25,7 @@
 #ifdef CONFIG_OMAP5_SEVM_PALMAS
 #include <linux/mfd/palmas.h>
 #endif
-
+#include <linux/i2c/smsc.h>
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -184,6 +184,91 @@ struct tsl2771_platform_data tsl2771_data = {
 	.gain_control                   = 0xE0,
 	.glass_attn                     = 0x01,
 	.device_factor                  = 0x34,
+};
+
+static uint32_t board_keymap[] = {
+
+	KEY(0, 0,  KEY_RESERVED)     , KEY(0, 1,  KEY_RESERVED),
+	KEY(0, 2,  KEY_F7)           , KEY(0, 3,  KEY_ESC),
+	KEY(0, 4,  KEY_F4)           , KEY(0, 5,  KEY_G),
+	KEY(0, 6,  KEY_RESERVED)     , KEY(0, 7,  KEY_H),
+	KEY(0, 8,  KEY_RESERVED)     , KEY(0, 9,  KEY_CYCLEWINDOWS),
+	KEY(0, 10, KEY_RESERVED)     , KEY(0, 11, KEY_RESERVED),
+	KEY(0, 12, KEY_BACKSPACE)    , KEY(0, 13, KEY_F11),
+	KEY(0, 14, KEY_FORWARD)      , KEY(0, 15, KEY_INSERT),
+
+	KEY(1, 0,  KEY_RIGHTSHIFT)   , KEY(1, 1,  KEY_RESERVED),
+	KEY(1, 2,  KEY_W)            , KEY(1, 3,  KEY_Q),
+	KEY(1, 4,  KEY_E)            , KEY(1, 5,  KEY_R),
+	KEY(1, 6,  KEY_RESERVED)     , KEY(1, 7,  KEY_U),
+	KEY(1, 8,  KEY_I)	     , KEY(1, 9,  KEY_RESERVED),
+	KEY(1, 10, KEY_RESERVED)     , KEY(1, 11, KEY_RESERVED),
+	KEY(1, 12, KEY_UP)           , KEY(1, 13, KEY_O),
+	KEY(1, 14, KEY_P)            , KEY(1, 15, KEY_LEFT),
+
+	KEY(2, 0,  KEY_RESERVED)     , KEY(2, 1,  KEY_RESERVED),
+	KEY(2, 2,  KEY_2)            , KEY(2, 3,  KEY_1),
+	KEY(2, 4,  KEY_3)            , KEY(2, 5,  KEY_4),
+	KEY(2, 6,  KEY_RESERVED)     , KEY(2, 7,  KEY_7),
+	KEY(2, 8,  KEY_8)            , KEY(2, 9,  KEY_RESERVED),
+	KEY(2, 10, KEY_RESERVED)     , KEY(2, 11, KEY_RIGHTALT),
+	KEY(2, 12, KEY_DOWN)         , KEY(2, 13, KEY_9),
+	KEY(2, 14, KEY_0)            , KEY(2, 15, KEY_RIGHT),
+
+	KEY(3, 0,  KEY_RESERVED)     , KEY(3, 1,  KEY_RIGHTCTRL),
+	KEY(3, 2,  KEY_S)            , KEY(3, 3,  KEY_A),
+	KEY(3, 4,  KEY_D)            , KEY(3, 5,  KEY_F),
+	KEY(3, 6,  KEY_RESERVED)     , KEY(3, 7,  KEY_J),
+	KEY(3, 8,  KEY_K)            , KEY(3, 9,  KEY_RESERVED),
+	KEY(3, 10, KEY_RESERVED)     , KEY(3, 11, KEY_RESERVED),
+	KEY(3, 12, KEY_ENTER)        , KEY(3, 13, KEY_L),
+	KEY(3, 14, KEY_SEMICOLON)    , KEY(3, 15, KEY_RESERVED),
+	KEY(4, 0,  KEY_LEFTSHIFT)    , KEY(4, 1,  KEY_RESERVED),
+	KEY(4, 2,  KEY_X)            , KEY(4, 3,  KEY_Z),
+	KEY(4, 4,  KEY_C)            , KEY(4, 5,  KEY_V),
+	KEY(4, 6,  KEY_RESERVED)     , KEY(4, 7,  KEY_M),
+	KEY(4, 8,  KEY_COMMA)        , KEY(4, 9,  KEY_RESERVED),
+	KEY(4, 10, KEY_RESERVED)     , KEY(4, 11, KEY_RESERVED),
+	KEY(4, 12, KEY_SPACE)        , KEY(4, 13, KEY_DOT),
+	KEY(4, 14, KEY_SLASH)        , KEY(4, 15, KEY_END),
+
+	KEY(5, 0,  KEY_RESERVED)     , KEY(5, 1,  KEY_LEFTCTRL),
+	KEY(5, 2,  KEY_F6)           , KEY(5, 3,  KEY_TAB),
+	KEY(5, 4,  KEY_F3)           , KEY(5, 5,  KEY_T),
+	KEY(5, 6,  KEY_RESERVED)     , KEY(5, 7,  KEY_Y),
+	KEY(5, 8,  KEY_LEFTBRACE)    , KEY(5, 9,  KEY_RESERVED),
+	KEY(5, 10, KEY_RESERVED)     , KEY(5, 11, KEY_RESERVED),
+	KEY(5, 12, KEY_RESERVED)     , KEY(5, 13, KEY_F10),
+	KEY(5, 14, KEY_RIGHTBRACE)   , KEY(5, 15, KEY_HOME),
+	KEY(6, 0,  KEY_RESERVED)     , KEY(6, 1,  KEY_RESERVED),
+	KEY(6, 2,  KEY_F5)           , KEY(6, 3,  KEY_RESERVED),
+	KEY(6, 4,  KEY_F2)           , KEY(6, 5,  KEY_5),
+	KEY(6, 6,  KEY_FN)           , KEY(6, 7,  KEY_6),
+	KEY(6, 8,  KEY_RESERVED)     , KEY(6, 9,  KEY_RESERVED),
+	KEY(6, 10, KEY_MENU)         , KEY(6, 11, KEY_RESERVED),
+	KEY(6, 12, KEY_BACKSLASH)    , KEY(6, 13, KEY_F9),
+	KEY(6, 14, KEY_RESERVED)     , KEY(6, 15, KEY_RESERVED),
+
+	KEY(7, 0,  KEY_RESERVED)     , KEY(7, 1,  KEY_RESERVED),
+	KEY(7, 2,  KEY_F8)           , KEY(7, 3,  KEY_CAPSLOCK),
+	KEY(7, 4,  KEY_F1)           , KEY(7, 5,  KEY_B),
+	KEY(7, 6,  KEY_RESERVED)     , KEY(7, 7,  KEY_N),
+	KEY(7, 8,  KEY_RESERVED)     , KEY(7, 9,  KEY_RESERVED),
+	KEY(7, 10, KEY_RESERVED)     , KEY(7, 11, KEY_LEFTALT),
+	KEY(7, 12, KEY_RESERVED)     , KEY(7, 13, KEY_F12),
+	KEY(7, 14, KEY_RESERVED)     , KEY(7, 15, KEY_DELETE),
+};
+
+static struct matrix_keymap_data board_map_data = {
+	.keymap                 = board_keymap,
+	.keymap_size            = ARRAY_SIZE(board_keymap),
+};
+
+static struct smsc_keypad_data omap5_kp_data = {
+	.keymap_data    = &board_map_data,
+	.rows           = 8,
+	.cols           = 16,
+	.rep            = 1,
 };
 
 static void __init omap_5430evm_init_early(void)
@@ -895,6 +980,14 @@ static struct i2c_board_info __initdata omap5evm_i2c_4_boardinfo[] = {
 	},
 };
 
+static struct i2c_board_info __initdata omap5evm_i2c_5_boardinfo[] = {
+	{
+		I2C_BOARD_INFO("smsc", 0x38),
+		.platform_data = &omap5_kp_data,
+		.irq = 151,
+	},
+};
+
 static int __init omap_5430evm_i2c_init(void)
 {
 	omap_i2c_hwspinlock_init(1, 0, &sdp4430_i2c_1_bus_pdata);
@@ -919,7 +1012,8 @@ static int __init omap_5430evm_i2c_init(void)
 	omap_register_i2c_bus(3, 400, NULL, 0);
 	omap_register_i2c_bus(4, 400, omap5evm_i2c_4_boardinfo,
 				ARRAY_SIZE(omap5evm_i2c_4_boardinfo));
-	omap_register_i2c_bus(5, 400, NULL, 0);
+	omap_register_i2c_bus(5, 400, omap5evm_i2c_5_boardinfo,
+				ARRAY_SIZE(omap5evm_i2c_5_boardinfo));
 	return 0;
 }
 
