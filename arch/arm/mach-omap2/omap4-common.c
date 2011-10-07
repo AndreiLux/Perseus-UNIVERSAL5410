89bed7c4093b4a14819f35eb68732fab6776c487
Santosh Shilimkar <santosh.shilimkar@ti.com>
2011-10-07 15:45:13 +0530

    ARM: OMAP5: PM: Update SAR RAM base addrees
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- arch/arm/mach-omap2/omap4-common.c
+++ arch/arm/mach-omap2/omap4-common.c
@@ -200,15 +200,17 @@ void __iomem *omap4_get_sar_ram_base(void)
  */
 static int __init omap4_sar_ram_init(void)
 {
-	/*
-	 * To avoid code running on other OMAPs in
-	 * multi-omap builds
-	 */
-	if (!cpu_is_omap44xx())
+	unsigned long sar_base_phys;
+
+	if (cpu_is_omap44xx())
+		sar_base_phys = OMAP44XX_SAR_RAM_BASE;
+	else if (cpu_is_omap54xx())
+		sar_base_phys = OMAP54XX_SAR_RAM_BASE;
+	else
 		return -ENOMEM;
 
 	/* Static mapping, never released */
-	sar_ram_base = ioremap(OMAP44XX_SAR_RAM_BASE, SZ_16K);
+	sar_ram_base = ioremap(sar_base_phys, SZ_16K);
 	if (WARN_ON(!sar_ram_base))
 		return -ENOMEM;
 
