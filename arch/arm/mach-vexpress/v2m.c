083c32bf6bed0772a8dfbf9029e74240f73f37af
Grant Likely <grant.likely@secretlab.ca>
2011-10-24 11:09:12 +0200

    arm/dt: vexpress: add basic DT platform matching support
    This patch adds a DT match table to the Versatile Express machine
    description in order to enable basic device tree support.
    
    Tested on a Versatile Express board where the device tree blob is
    passed to the kernel by u-boot.
    
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    [converted .dts file to use skeleton.dtsi, and added 'dtbs' targets]
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    
    Conflicts:
    
    	arch/arm/mach-vexpress/v2m.c

--- arch/arm/mach-vexpress/v2m.c
+++ arch/arm/mach-vexpress/v2m.c
@@ -442,6 +442,11 @@ static void __init v2m_init(void)
 	ct_desc->init_tile();
 }
 
+static const char *vexpress_dt_match[] __initdata = {
+	"arm,vexpress",
+	NULL,
+};
+
 MACHINE_START(VEXPRESS, "ARM-Versatile Express")
 	.atag_offset	= 0x100,
 	.map_io		= v2m_map_io,
@@ -451,4 +456,5 @@ MACHINE_START(VEXPRESS, "ARM-Versatile Express")
 	.handle_irq	= gic_handle_irq,
 	.init_machine	= v2m_init,
 	.restart	= v2m_restart,
+	.dt_compat	= vexpress_dt_match,
 MACHINE_END
