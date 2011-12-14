f2aa522ac61829f5777393e5aec9b5fb810dfcf1
Mike Turquette <mturquette@ti.com>
2011-12-14 13:33:32 -0800

    omap5: clk: .round_rate & .recalc for DPLL_ABE
    Like OMAP4, the ABE DPLL in OMAP5 supports the REGM4XEN bit to multiply
    the output rate by 4.  As such it's .recalc and .round_rate must take
    this conditional math into account.
    
    Updates dpll_abe_ck to use omap4_dpll_regm4xen_round_rate and
    omap4_dpll_regm4xen_recalc instead of the usual omap3 variants used by
    all other non-CORE DPLLs.
    
    Signed-off-by: Mike Turquette <mturquette@ti.com>

--- arch/arm/mach-omap2/clock54xx_data.c
+++ arch/arm/mach-omap2/clock54xx_data.c
@@ -29,6 +29,7 @@
 #include <plat/clkdev_omap.h>
 
 #include "clock.h"
+#include "clock44xx.h"
 #include "clock54xx.h"
 #include "cm1_54xx.h"
 #include "cm2_54xx.h"
@@ -246,8 +247,8 @@ static struct clk dpll_abe_ck = {
 	.dpll_data	= &dpll_abe_dd,
 	.init		= &omap2_init_dpll_parent,
 	.ops		= &clkops_omap3_noncore_dpll_ops,
-	.recalc		= &omap3_dpll_recalc,
-	.round_rate	= &omap2_dpll_round_rate,
+	.recalc		= &omap4_dpll_regm4xen_recalc,
+	.round_rate	= &omap4_dpll_regm4xen_round_rate,
 	.set_rate	= &omap3_noncore_dpll_set_rate,
 };
 
