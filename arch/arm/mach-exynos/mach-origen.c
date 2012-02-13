b099d13342368dc79a13bf63fed122fb8911010b
Tushar Behera <tushar.behera@linaro.org>
2012-02-13 14:36:28 +0530

    ARM: EXYNOS: Add USB OTG support for Origen board
    USB-OTG support has been added using s3c-hsotg driver.
    
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>

--- arch/arm/mach-exynos/mach-origen.c
+++ arch/arm/mach-exynos/mach-origen.c
@@ -41,6 +41,7 @@
 #include <plat/pd.h>
 #include <plat/fb.h>
 #include <plat/mfc.h>
+#include <plat/udc-hs.h>
 
 #include <mach/ohci.h>
 #include <mach/map.h>
@@ -500,6 +501,9 @@ static void __init origen_ohci_init(void)
 	exynos4_ohci_set_platdata(pdata);
 }
 
+/* USB OTG */
+static struct s3c_hsotg_plat origen_hsotg_pdata;
+
 static struct gpio_keys_button origen_gpio_keys_table[] = {
 	{
 		.code			= KEY_MENU,
@@ -629,6 +633,7 @@ static struct platform_device *origen_devices[] __initdata = {
 	&s3c_device_i2c0,
 	&s3c_device_rtc,
 	&s3c_device_wdt,
+	&s3c_device_usb_hsotg,
 	&s5p_device_ehci,
 	&s5p_device_fimc0,
 	&s5p_device_fimc1,
@@ -714,6 +719,7 @@ static void __init origen_machine_init(void)
 
 	origen_ehci_init();
 	origen_ohci_init();
+	s3c_hsotg_set_platdata(&origen_hsotg_pdata);
 	clk_xusbxti.rate = 24000000;
 
 	s5p_tv_setup();
