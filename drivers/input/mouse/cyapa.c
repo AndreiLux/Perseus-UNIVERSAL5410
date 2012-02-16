78ea98863a79b9adc53a4edddf32013bc8d8fdf6
Benson Leung <bleung@chromium.org>
2012-02-16 02:12:41 -0800

    CHROMIUM: Input: cyapa - retry power mode writes
    There are some conditions where the trackpad is unresponsive to a power
    mode transition, especially when the trackpad is in Off state.
    Allow for 3 retries with a 10ms delay only if the write fails.
    
    BUG=chrome-os-partner:8070
    TEST=Suspend and resume the system a number of times. Ensure that on resume
    the trackpad is always responsive.
    
    Change-Id: I07153873cc68e046310bdd831a7740557d620a1b
    Signed-off-by: Benson Leung <bleung@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/16018
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>

--- drivers/input/mouse/cyapa.c
+++ drivers/input/mouse/cyapa.c
@@ -141,6 +141,7 @@
 #define PWR_MODE_MEDIUM_SLEEP  1
 #define PWR_MODE_DEEP_SLEEP    0
 #define SET_POWER_MODE_DELAY   10000  /* unit: us */
+#define SET_POWER_MODE_TRIES   3
 
 /*
  * CYAPA trackpad device states.
@@ -747,22 +748,27 @@ static int cyapa_bl_exit(struct cyapa *cyapa)
  */
 static int cyapa_set_power_mode(struct cyapa *cyapa, u8 power_mode)
 {
+	struct device *dev = &cyapa->client->dev;
 	int ret;
 	u8 power;
-	int tries = 3;
+	int tries = SET_POWER_MODE_TRIES;
 
 	if (cyapa->state != CYAPA_STATE_OP)
 		return 0;
 
 	power = cyapa_read_byte(cyapa, CYAPA_CMD_POWER_MODE);
 	power &= ~OP_POWER_MODE_MASK;
-	power |= ((power_mode << OP_POWER_MODE_SHIFT) & OP_POWER_MODE_MASK);
-	do {
+	power |= (power_mode << OP_POWER_MODE_SHIFT) & OP_POWER_MODE_MASK;
+	while (true) {
 		ret = cyapa_write_byte(cyapa, CYAPA_CMD_POWER_MODE, power);
-		/* sleep at least 10 ms. */
+		if (!ret || --tries < 1)
+			break;
+		dev_dbg(dev, "set power mode retry. tries left = %d\n", tries);
 		usleep_range(SET_POWER_MODE_DELAY, 2 * SET_POWER_MODE_DELAY);
-	} while ((ret != 0) && (tries-- > 0));
-
+	}
+	if (ret < 0)
+		dev_err(dev, "failed to set power_mode 0x%02x err = %d\n",
+			power_mode, ret);
 	return ret;
 }
 
