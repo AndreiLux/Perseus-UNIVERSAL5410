ef37a6985886c69d3ec6789d70e50f9b9cb123f9
Eunki Kim <eunki_kim@samsung.com>
2012-02-17 10:39:38 +0900

    ARM: EXYNOS: Add POWER key platform data on SMDK5250
    Signed-off-by: Eunki Kim <eunki_kim@samsung.com>

--- arch/arm/mach-exynos/mach-smdk5250.c
+++ arch/arm/mach-exynos/mach-smdk5250.c
@@ -19,6 +19,8 @@
 #include <linux/regulator/fixed.h>
 #include <linux/mfd/wm8994/pdata.h>
 #include <linux/pwm_backlight.h>
+#include <linux/input.h>
+#include <linux/gpio_event.h>
 
 #include <video/platform_lcd.h>
 
@@ -93,6 +95,57 @@ static struct s3c2410_uartcfg smdk5250_uartcfgs[] __initdata = {
 	},
 };
 
+static struct gpio_event_direct_entry smdk5250_keypad_key_map[] = {
+	{
+		.gpio   = EXYNOS5_GPX0(0),
+		.code   = KEY_POWER,
+	}
+};
+
+static struct gpio_event_input_info smdk5250_keypad_key_info = {
+	.info.func              = gpio_event_input_func,
+	.info.no_suspend        = true,
+	.debounce_time.tv64	= 5 * NSEC_PER_MSEC,
+	.type                   = EV_KEY,
+	.keymap                 = smdk5250_keypad_key_map,
+	.keymap_size            = ARRAY_SIZE(smdk5250_keypad_key_map)
+};
+
+static struct gpio_event_info *smdk5250_input_info[] = {
+	&smdk5250_keypad_key_info.info,
+};
+
+static struct gpio_event_platform_data smdk5250_input_data = {
+	.names  = {
+		"smdk5250-keypad",
+		NULL,
+	},
+	.info           = smdk5250_input_info,
+	.info_count     = ARRAY_SIZE(smdk5250_input_info),
+};
+
+static struct platform_device smdk5250_input_device = {
+	.name   = GPIO_EVENT_DEV_NAME,
+	.id     = 0,
+	.dev    = {
+		.platform_data = &smdk5250_input_data,
+	},
+};
+
+static void __init smdk5250_gpio_power_init(void)
+{
+	int err = 0;
+
+	err = gpio_request_one(EXYNOS5_GPX0(0), 0, "GPX0");
+	if (err) {
+		printk(KERN_ERR "failed to request GPX0 for "
+				"suspend/resume control\n");
+		return;
+	}
+	s3c_gpio_setpull(EXYNOS5_GPX0(0), S3C_GPIO_PULL_NONE);
+
+	gpio_free(EXYNOS5_GPX0(0));
+}
 static struct regulator_consumer_supply wm8994_fixed_voltage0_supplies[] = {
 	REGULATOR_SUPPLY("AVDD2", "1-001a"),
 	REGULATOR_SUPPLY("CPVDD", "1-001a"),
@@ -730,6 +783,7 @@ static struct platform_device *smdk5250_devices[] __initdata = {
 	&SYSMMU_PLATDEV(is_dis0),
 	&SYSMMU_PLATDEV(is_dis1),
 	&SYSMMU_PLATDEV(is_3dnr),
+	&smdk5250_input_device,
 #ifdef CONFIG_ION_EXYNOS
 	&exynos_device_ion,
 #endif
@@ -856,10 +910,10 @@ static void __init smdk5250_machine_init(void)
 	dev_set_name(&s5p_device_fimd1.dev, "exynos5-fb.1");
 	clk_add_alias("lcd", "exynos5-fb.1", "fimd", &s5p_device_fimd1.dev);
 #endif
-
 #ifdef CONFIG_FB_MIPI_DSIM
 	s5p_dsim_set_platdata(&dsim_platform_data);
 #endif
+	smdk5250_gpio_power_init();
 
 	platform_add_devices(smdk5250_devices, ARRAY_SIZE(smdk5250_devices));
 
