701e5df64942eebc09efabe4f3ee341353b9fd73
Janghyuck Kim <janghyuck.kim@samsung.com>
2012-02-22 19:49:06 +0900

    media: exynos-mfc: add exynos MFC driver for EXYNOS5 SoC
    This patch adds MFC driver for EXYNOS5 SoC and updates
    for v3.3.
    
    Signed-off-by: Janghyuck Kim <janghyuck.kim@samsung.com>

---
+++ drivers/media/video/exynos/mfc/s5p_mfc_mem.c
@@ -0,0 +1,238 @@
+/*
+ * linux/drivers/media/video/exynos/mfc/s5p_mfc_mem.c
+ *
+ * Copyright (c) 2010 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <linux/dma-mapping.h>
+#include <media/videobuf2-core.h>
+#include <media/videobuf2-memops.h>
+#include <asm/cacheflush.h>
+
+#include "s5p_mfc_common.h"
+#include "s5p_mfc_mem.h"
+#include "s5p_mfc_pm.h"
+#include "s5p_mfc_debug.h"
+
+#define	MFC_ION_NAME	"s5p-mfc"
+
+#if defined(CONFIG_VIDEOBUF2_CMA_PHYS)
+static const char * const s5p_mem_types[] = {
+	MFC_CMA_FW,
+	MFC_CMA_BANK1,
+	MFC_CMA_BANK2,
+};
+
+static unsigned long s5p_mem_alignments[] = {
+	MFC_CMA_FW_ALIGN,
+	MFC_CMA_BANK1_ALIGN,
+	MFC_CMA_BANK2_ALIGN,
+};
+
+struct vb2_mem_ops *s5p_mfc_mem_ops(void)
+{
+	return (struct vb2_mem_ops *)&vb2_cma_phys_memops;
+}
+
+void **s5p_mfc_mem_init_multi(struct device *dev, unsigned int ctx_num)
+{
+/* TODO Cachable should be set */
+	return (void **)vb2_cma_phys_init_multi(dev, ctx_num,
+					   s5p_mem_types,
+					   s5p_mem_alignments, 0);
+}
+
+void s5p_mfc_mem_cleanup_multi(void **alloc_ctxes)
+{
+	vb2_cma_phys_cleanup_multi(alloc_ctxes);
+}
+#elif defined(CONFIG_VIDEOBUF2_ION)
+struct vb2_mem_ops *s5p_mfc_mem_ops(void)
+{
+	return (struct vb2_mem_ops *)&vb2_ion_memops;
+}
+
+void **s5p_mfc_mem_init_multi(struct device *dev, unsigned int ctx_num)
+{
+	struct vb2_ion ion;
+	void **alloc_ctxes;
+	struct vb2_drv vb2_drv;
+	struct s5p_mfc_dev *m_dev = platform_get_drvdata(to_platform_device(dev));
+
+	/* TODO */
+	ion.name = MFC_ION_NAME;
+	ion.dev = dev;
+	ion.cacheable = true;
+	ion.align = IS_MFCV6(m_dev) ? SZ_4K : SZ_128K;
+	ion.contig = false;
+
+	vb2_drv.use_mmu = true;
+
+	s5p_mfc_power_on();
+	alloc_ctxes = (void **)vb2_ion_init_multi(ctx_num, &ion,
+							&vb2_drv);
+	s5p_mfc_power_off();
+
+	return alloc_ctxes;
+}
+
+void s5p_mfc_mem_cleanup_multi(void **alloc_ctxes)
+{
+	vb2_ion_cleanup_multi(alloc_ctxes);
+}
+#endif
+
+#if defined(CONFIG_VIDEOBUF2_CMA_PHYS)
+struct vb2_cma_phys_conf {
+	struct device		*dev;
+	const char		*type;
+	unsigned long		alignment;
+	bool			cacheable;
+};
+
+struct vb2_cma_phys_buf {
+	struct vb2_cma_phys_conf		*conf;
+	dma_addr_t			paddr;
+	unsigned long			size;
+	struct vm_area_struct		*vma;
+	atomic_t			refcount;
+	struct vb2_vmarea_handler	handler;
+	bool				cacheable;
+};
+void s5p_mfc_cache_clean(void *alloc_ctx)
+{
+	struct vb2_cma_phys_buf *buf = (struct vb2_cma_phys_buf *)alloc_ctx;
+	void *start_addr;
+	unsigned long size;
+	unsigned long paddr = (dma_addr_t)buf->paddr;
+
+	start_addr = (dma_addr_t *)phys_to_virt(buf->paddr);
+	size = buf->size;
+
+	dmac_map_area(start_addr, size, DMA_TO_DEVICE);
+	outer_clean_range(paddr, paddr + size);
+}
+
+void s5p_mfc_cache_inv(void *alloc_ctx)
+{
+	struct vb2_cma_phys_buf *buf = (struct vb2_cma_phys_buf *)alloc_ctx;
+	void *start_addr;
+	unsigned long size;
+	unsigned long paddr = (dma_addr_t)buf->paddr;
+
+	start_addr = (dma_addr_t *)phys_to_virt(buf->paddr);
+	size = buf->size;
+
+	outer_inv_range(paddr, paddr + size);
+	dmac_unmap_area(start_addr, size, DMA_FROM_DEVICE);
+}
+
+void s5p_mfc_mem_suspend(void *alloc_ctx)
+{
+	/* NOP */
+}
+
+void s5p_mfc_mem_resume(void *alloc_ctx)
+{
+	/* NOP */
+}
+
+void s5p_mfc_mem_set_cacheable(void *alloc_ctx, bool cacheable)
+{
+	vb2_cma_phys_set_cacheable(alloc_ctx, cacheable);
+}
+
+void s5p_mfc_mem_get_cacheable(void *alloc_ctx)
+{
+	/* NOP */
+}
+
+int s5p_mfc_mem_cache_flush(struct vb2_buffer *vb, u32 plane_no)
+{
+	vb2_cma_phys_cache_flush(vb, plane_no);
+	return 0;
+}
+#elif defined(CONFIG_VIDEOBUF2_ION)
+struct vb2_ion_conf {
+	struct device		*dev;
+	const char		*name;
+
+	struct ion_client	*client;
+
+	unsigned long		align;
+	bool			contig;
+	bool			sharable;
+	bool			cacheable;
+	bool			use_mmu;
+	atomic_t		mmu_enable;
+
+	spinlock_t		slock;
+};
+
+struct vb2_ion_buf {
+	struct vm_area_struct		**vma;
+	int				vma_count;
+	struct vb2_ion_conf		*conf;
+	struct vb2_vmarea_handler	handler;
+
+	struct ion_handle		*handle;	/* Kernel space */
+
+	dma_addr_t			kva;
+	dma_addr_t			dva;
+	unsigned long			size;
+
+	struct scatterlist		*sg;
+	int				nents;
+
+	atomic_t			ref;
+
+	bool				cacheable;
+};
+
+void s5p_mfc_cache_clean(void *alloc_ctx)
+{
+	struct vb2_ion_buf *buf = (struct vb2_ion_buf *)alloc_ctx;
+
+	dma_sync_sg_for_device(buf->conf->dev, buf->sg, buf->nents,
+		DMA_TO_DEVICE);
+}
+
+void s5p_mfc_cache_inv(void *alloc_ctx)
+{
+	struct vb2_ion_buf *buf = (struct vb2_ion_buf *)alloc_ctx;
+
+	dma_sync_sg_for_cpu(buf->conf->dev, buf->sg, buf->nents,
+		DMA_FROM_DEVICE);
+}
+
+void s5p_mfc_mem_suspend(void *alloc_ctx)
+{
+	vb2_ion_suspend(alloc_ctx);
+}
+
+void s5p_mfc_mem_resume(void *alloc_ctx)
+{
+	vb2_ion_resume(alloc_ctx);
+}
+
+void s5p_mfc_mem_set_cacheable(void *alloc_ctx, bool cacheable)
+{
+	vb2_ion_set_cacheable(alloc_ctx, cacheable);
+}
+
+void s5p_mfc_mem_get_cacheable(void *alloc_ctx)
+{
+	vb2_ion_get_cacheable(alloc_ctx);
+}
+
+int s5p_mfc_mem_cache_flush(struct vb2_buffer *vb, u32 plane_no)
+{
+	return vb2_ion_cache_flush(vb, plane_no);
+}
+#endif
---
+++ drivers/media/video/exynos/mfc/s5p_mfc_pm.c
@@ -0,0 +1,181 @@
+/*
+ * linux/drivers/media/video/exynos/mfc/s5p_mfc_pm.c
+ *
+ * Copyright (c) 2010 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <linux/err.h>
+#include <linux/clk.h>
+#include <linux/pm_runtime.h>
+#include <linux/platform_device.h>
+
+#include <mach/exynos-mfc.h>
+
+#include "s5p_mfc_common.h"
+#include "s5p_mfc_debug.h"
+#include "s5p_mfc_pm.h"
+
+#define CLK_DEBUG
+
+static struct s5p_mfc_pm *pm;
+atomic_t clk_ref;
+
+#if defined(CONFIG_ARCH_EXYNOS4)
+
+#define MFC_PARENT_CLK_NAME	"mout_mfc0"
+#define MFC_CLKNAME		"sclk_mfc"
+#define MFC_GATE_CLK_NAME	"mfc"
+
+int s5p_mfc_init_pm(struct s5p_mfc_dev *dev)
+{
+	struct clk *parent, *sclk;
+	int ret = 0;
+
+	pm = &dev->pm;
+
+	parent = clk_get(&dev->plat_dev->dev, MFC_PARENT_CLK_NAME);
+	if (IS_ERR(parent)) {
+		printk(KERN_ERR "failed to get parent clock\n");
+		ret = -ENOENT;
+		goto err_p_clk;
+	}
+
+	sclk = clk_get(&dev->plat_dev->dev, MFC_CLKNAME);
+	if (IS_ERR(sclk)) {
+		printk(KERN_ERR "failed to get source clock\n");
+		ret = -ENOENT;
+		goto err_s_clk;
+	}
+
+	clk_set_parent(sclk, parent);
+	clk_set_rate(sclk, 200 * 1000000);
+
+	/* clock for gating */
+	pm->clock = clk_get(&dev->plat_dev->dev, MFC_GATE_CLK_NAME);
+	if (IS_ERR(pm->clock)) {
+		printk(KERN_ERR "failed to get clock-gating control\n");
+		ret = -ENOENT;
+		goto err_g_clk;
+	}
+
+	atomic_set(&pm->power, 0);
+	atomic_set(&clk_ref, 0);
+
+	pm->device = &dev->plat_dev->dev;
+	pm_runtime_enable(pm->device);
+
+	return 0;
+
+err_g_clk:
+	clk_put(sclk);
+err_s_clk:
+	clk_put(parent);
+err_p_clk:
+	return ret;
+}
+
+#elif defined(CONFIG_ARCH_EXYNOS5)
+
+#define MFC_PARENT_CLK_NAME	"aclk_333"
+#define MFC_CLKNAME		"sclk_mfc"
+#define MFC_GATE_CLK_NAME	"mfc"
+
+int s5p_mfc_init_pm(struct s5p_mfc_dev *dev)
+{
+	struct clk *parent_clk;
+	int ret = 0;
+	struct s5p_mfc_platdata *pdata;
+
+	pm = &dev->pm;
+
+	/* clock for gating */
+	pm->clock = clk_get(&dev->plat_dev->dev, MFC_GATE_CLK_NAME);
+	if (IS_ERR(pm->clock)) {
+		printk(KERN_ERR "failed to get clock-gating control\n");
+		ret = PTR_ERR(pm->clock);
+		goto err_g_clk;
+	}
+
+	parent_clk = clk_get(&dev->plat_dev->dev, MFC_PARENT_CLK_NAME);
+	if (IS_ERR(parent_clk)) {
+		printk(KERN_ERR "failed to get parent clock %s.\n", MFC_PARENT_CLK_NAME);
+		ret = PTR_ERR(parent_clk);
+		goto err_p_clk;
+	}
+
+	pdata = dev->platdata;
+	clk_set_rate(parent_clk, pdata->clock_rate);
+
+	atomic_set(&pm->power, 0);
+	atomic_set(&clk_ref, 0);
+
+	pm->device = &dev->plat_dev->dev;
+	pm_runtime_enable(pm->device);
+
+	clk_put(parent_clk);
+
+	return 0;
+
+err_p_clk:
+	clk_put(pm->clock);
+err_g_clk:
+	return ret;
+}
+
+#endif
+
+void s5p_mfc_final_pm(struct s5p_mfc_dev *dev)
+{
+	clk_put(pm->clock);
+
+	pm_runtime_disable(pm->device);
+}
+
+int s5p_mfc_clock_on(void)
+{
+	int ret;
+	struct s5p_mfc_dev *dev = platform_get_drvdata(to_platform_device(pm->device));
+
+	atomic_inc(&clk_ref);
+
+	mfc_debug(3, "+ %d", atomic_read(&clk_ref));
+
+	ret = clk_enable(pm->clock);
+	if (atomic_read(&clk_ref) == 1)
+		s5p_mfc_mem_resume(dev->alloc_ctx[0]);
+
+	return ret;
+}
+
+void s5p_mfc_clock_off(void)
+{
+	struct s5p_mfc_dev *dev = platform_get_drvdata(to_platform_device(pm->device));
+
+	atomic_dec(&clk_ref);
+
+	mfc_debug(3, "- %d", atomic_read(&clk_ref));
+
+	if (atomic_read(&clk_ref) == 0)
+		s5p_mfc_mem_suspend(dev->alloc_ctx[0]);
+	clk_disable(pm->clock);
+}
+
+int s5p_mfc_power_on(void)
+{
+	atomic_set(&pm->power, 1);
+
+	return pm_runtime_get_sync(pm->device);
+}
+
+int s5p_mfc_power_off(void)
+{
+	atomic_set(&pm->power, 0);
+
+	return pm_runtime_put_sync(pm->device);
+}
