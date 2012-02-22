452d61a30d2975e133e441070895000ae0843913
J Keerthy <j-keerthy@ti.com>
2012-02-22 19:56:34 +0800

    OMAP4: Add Plamas support in OMAP4 Board files
    Define a new config entry for Palmas and Add Palmas specific OMAP4 SDP
    Board file changes.
    
    Signed-off-by: J Keerthy <j-keerthy@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -50,6 +50,13 @@
 #include "control.h"
 #include "common-board-devices.h"
 
+#ifdef CONFIG_OMAP4460_SEVM_PALMAS
+#include <linux/gpio.h>
+#include <linux/regulator/machine.h>
+#include <linux/regulator/fixed.h>
+#include <linux/mfd/palmas.h>
+#endif
+
 #define ETH_KS8851_IRQ			34
 #define ETH_KS8851_POWER_ON		48
 #define ETH_KS8851_QUART		138
@@ -426,6 +433,7 @@ static struct omap_musb_board_data musb_board_data = {
 	.power			= 100,
 };
 
+#ifndef CONFIG_OMAP4460_SEVM_PALMAS
 static struct omap2_hsmmc_info mmc[] = {
 	{
 		.mmc		= 2,
@@ -457,6 +465,7 @@ static struct omap2_hsmmc_info mmc[] = {
 static struct regulator_consumer_supply sdp4430_vaux_supply[] = {
 	REGULATOR_SUPPLY("vmmc", "omap_hsmmc.1"),
 };
+#endif
 
 static struct regulator_consumer_supply omap4_sdp4430_vmmc5_supply = {
 	.supply = "vmmc",
@@ -489,6 +498,448 @@ static struct platform_device omap_vwlan_device = {
 	},
 };
 
+#ifdef CONFIG_OMAP4460_SEVM_PALMAS
+#define OMAP5_GPIO_END	0
+
+static struct palmas_gpadc_platform_data omap5_palmas_gpadc = {
+	.ch3_current = 0,
+	.ch0_current = 0,
+	.bat_removal = 0,
+	.start_polarity = 0,
+};
+
+/* Initialisation Data for Regulators */
+
+static struct palmas_reg_init omap5_smps12_init = {
+	.warm_reset = 0,
+	.roof_floor = 0,
+	.mode_sleep = 0,
+	.tstep = 0,
+};
+
+static struct palmas_reg_init omap5_smps45_init = {
+	.warm_reset = 0,
+	.roof_floor = 0,
+	.mode_sleep = 0,
+	.tstep = 0,
+};
+
+static struct palmas_reg_init omap5_smps6_init = {
+	.warm_reset = 0,
+	.roof_floor = 0,
+	.mode_sleep = 1,
+	.tstep = 0,
+};
+
+static struct palmas_reg_init omap5_smps7_init = {
+	.warm_reset = 0,
+	.roof_floor = 0,
+	.mode_sleep = 1,
+};
+
+static struct palmas_reg_init omap5_smps8_init = {
+	.warm_reset = 0,
+	.roof_floor = 0,
+	.mode_sleep = 0,
+	.tstep = 0,
+};
+
+static struct palmas_reg_init omap5_smps9_init = {
+	.warm_reset = 0,
+	.roof_floor = 0,
+	.mode_sleep = 0,
+	.vsel = 0xbd,
+};
+
+static struct palmas_reg_init omap5_smps10_init = {
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo1_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo2_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo3_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo4_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo5_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo6_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo7_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo8_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldo9_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldoln_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init omap5_ldousb_init = {
+	.warm_reset = 0,
+	.mode_sleep = 0,
+};
+
+static struct palmas_reg_init *palmas_omap_reg_init[] = {
+	&omap5_smps12_init,
+	NULL, /* SMPS123 not used in this configuration */
+	NULL, /* SMPS3 not used in this configuration */
+	&omap5_smps45_init,
+	NULL, /* SMPS457 not used in this configuration */
+	&omap5_smps6_init,
+	&omap5_smps7_init,
+	&omap5_smps8_init,
+	&omap5_smps9_init,
+	&omap5_smps10_init,
+	&omap5_ldo1_init,
+	&omap5_ldo2_init,
+	&omap5_ldo3_init,
+	&omap5_ldo4_init,
+	&omap5_ldo5_init,
+	&omap5_ldo6_init,
+	&omap5_ldo7_init,
+	&omap5_ldo8_init,
+	&omap5_ldo9_init,
+	&omap5_ldoln_init,
+	&omap5_ldousb_init,
+
+};
+
+/* Constraints for Regulators */
+static struct regulator_init_data omap5_smps12 = {
+	.constraints = {
+		.min_uV			= 600000,
+		.max_uV			= 1310000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_VOLTAGE
+					| REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_smps45 = {
+	.constraints = {
+		.min_uV			= 600000,
+		.max_uV			= 1310000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_VOLTAGE
+					| REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_smps6 = {
+	.constraints = {
+		.min_uV			= 1200000,
+		.max_uV			= 1200000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_smps7 = {
+	.constraints = {
+		.min_uV			= 1800000,
+		.max_uV			= 1800000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_smps8 = {
+	.constraints = {
+		.min_uV			= 600000,
+		.max_uV			= 1310000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_VOLTAGE
+					| REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_consumer_supply omap5_adac_supply[] = {
+	REGULATOR_SUPPLY("vcc", "soc-audio"),
+};
+
+static struct regulator_init_data omap5_smps9 = {
+	.constraints = {
+		.min_uV			= 2100000,
+		.max_uV			= 2100000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+	.num_consumer_supplies	= ARRAY_SIZE(omap5_adac_supply),
+	.consumer_supplies	= omap5_adac_supply,
+
+};
+
+static struct regulator_consumer_supply omap5_vbus_supply[] = {
+	REGULATOR_SUPPLY("vbus", "1-0048"),
+};
+
+static struct regulator_init_data omap5_smps10 = {
+	.constraints = {
+		.min_uV			= 5000000,
+		.max_uV			= 5000000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+	.num_consumer_supplies	= ARRAY_SIZE(omap5_vbus_supply),
+	.consumer_supplies	= omap5_vbus_supply,
+};
+
+static struct regulator_init_data omap5_ldo1 = {
+	.constraints = {
+		.min_uV			= 2800000,
+		.max_uV			= 2800000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldo2 = {
+	.constraints = {
+		.min_uV			= 2800000,
+		.max_uV			= 2800000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldo3 = {
+	.constraints = {
+		.min_uV			= 3000000,
+		.max_uV			= 3000000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldo4 = {
+	.constraints = {
+		.min_uV			= 2200000,
+		.max_uV			= 2200000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldo5 = {
+	.constraints = {
+		.min_uV			= 1800000,
+		.max_uV			= 1800000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldo6 = {
+	.constraints = {
+		.min_uV			= 1500000,
+		.max_uV			= 1500000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldo7 = {
+	.constraints = {
+		.min_uV			= 1500000,
+		.max_uV			= 1500000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldo8 = {
+	.constraints = {
+		.min_uV			= 1500000,
+		.max_uV			= 1500000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldo9 = {
+	.constraints = {
+		.min_uV			= 1800000,
+		.max_uV			= 1800000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldoln = {
+	.constraints = {
+		.min_uV			= 1800000,
+		.max_uV			= 1800000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data omap5_ldousb = {
+	.constraints = {
+		.min_uV			= 3250000,
+		.max_uV			= 3250000,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+};
+
+static struct regulator_init_data *palmas_omap5_reg[] = {
+	&omap5_smps12,
+	NULL, /* SMPS123 not used in this configuration */
+	NULL, /* SMPS3 not used in this configuration */
+	&omap5_smps45,
+	NULL, /* SMPS457 not used in this configuration */
+	&omap5_smps6,
+	&omap5_smps7,
+	&omap5_smps8,
+	&omap5_smps9,
+	&omap5_smps10,
+
+	&omap5_ldo1,
+	&omap5_ldo2,
+	&omap5_ldo3,
+	&omap5_ldo4,
+	&omap5_ldo5,
+	&omap5_ldo6,
+	&omap5_ldo7,
+	&omap5_ldo8,
+	&omap5_ldo9,
+	&omap5_ldoln,
+	&omap5_ldousb,
+};
+
+static struct palmas_pmic_platform_data omap5_palmas_pmic = {
+	.reg_data = palmas_omap5_reg,
+	.reg_init = palmas_omap_reg_init,
+
+	.ldo6_vibrator = 0,
+};
+
+static struct palmas_resource_platform_data omap5_palmas_resource = {
+	.clk32kg_mode_sleep = 0,
+	.clk32kgaudio_mode_sleep = 0,
+	.regen1_mode_sleep = 0,
+	.regen2_mode_sleep = 0,
+	.sysen1_mode_sleep = 0,
+	.sysen2_mode_sleep = 0,
+
+	.nsleep_res = 0,
+	.nsleep_smps = 0,
+	.nsleep_ldo1 = 0,
+	.nsleep_ldo2 = 0,
+
+	.enable1_res = 0,
+	.enable1_smps = 0,
+	.enable1_ldo1 = 0,
+	.enable1_ldo2 = 0,
+
+	.enable2_res = 0,
+	.enable2_smps = 0,
+	.enable2_ldo1 = 0,
+	.enable2_ldo2 = 0,
+};
+
+static struct palmas_usb_platform_data omap5_palmas_usb = {
+	.wakeup = 1,
+};
+
+static struct palmas_platform_data palmas_omap5 = {
+	.gpio_base = OMAP5_GPIO_END,
+
+	.power_ctrl = POWER_CTRL_NSLEEP_MASK | POWER_CTRL_ENABLE1_MASK |
+			POWER_CTRL_ENABLE1_MASK,
+
+	.gpadc_pdata = &omap5_palmas_gpadc,
+	.pmic_pdata = &omap5_palmas_pmic,
+	.usb_pdata = &omap5_palmas_usb,
+	.resource_pdata = &omap5_palmas_resource,
+};
+
+static struct i2c_board_info __initdata palmas_i2c[] = {
+	{
+		I2C_BOARD_INFO("twl6035", 0x48),
+		.platform_data = &palmas_omap5,
+	}
+};
+
+int palmas_i2c_init(void)
+{
+	palmas_i2c[0].irq = OMAP44XX_IRQ_SYS_1N;
+
+	omap_register_i2c_bus(1, 400, palmas_i2c, ARRAY_SIZE(palmas_i2c));
+
+	return 0;
+}
+#endif
+
+#ifndef CONFIG_OMAP4460_SEVM_PALMAS
 static int omap4_twl6030_hsmmc_late_init(struct device *dev)
 {
 	int irq = 0;
@@ -625,6 +1076,7 @@ static int __init omap4_i2c_init(void)
 				ARRAY_SIZE(sdp4430_i2c_4_boardinfo));
 	return 0;
 }
+#endif
 
 static void __init omap_sfh7741prox_init(void)
 {
@@ -945,13 +1397,28 @@ static void __init omap_4430sdp_init(void)
 
 	omap4_mux_init(board_mux, NULL, package);
 
+	omap_board_config = sdp4430_config;
+	omap_board_config_size = ARRAY_SIZE(sdp4430_config);
+
+#ifndef CONFIG_OMAP4460_SEVM_PALMAS
 	omap4_i2c_init();
+	omap4_twl6030_hsmmc_init(mmc);
+#endif
 	omap_sfh7741prox_init();
 	platform_add_devices(sdp4430_devices, ARRAY_SIZE(sdp4430_devices));
 	omap_serial_init();
 	omap_sdrc_init(NULL, NULL);
 	omap4_sdp4430_wifi_init();
-	omap4_twl6030_hsmmc_init(mmc);
+#ifdef CONFIG_OMAP4460_SEVM_PALMAS
+	palmas_i2c_init();
+
+	/*
+	 * Drive MSECURE high for TWL6030 write access.
+	*/
+	omap_mux_init_signal("fref_clk0_out.gpio_wk6", OMAP_PIN_OUTPUT);
+	gpio_request(6, "msecure");
+	gpio_direction_output(6, 1);
+#endif
 
 	usb_musb_init(&musb_board_data);
 
