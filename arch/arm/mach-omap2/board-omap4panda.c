4e85847271a619cf00029862e9e361a965d4fcf1
Tero Kristo <t-kristo@ti.com>
2012-02-23 16:32:29 +0800

    omap4panda: fix i2c access
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -24,6 +24,7 @@
 #include <linux/leds.h>
 #include <linux/gpio.h>
 #include <linux/usb/otg.h>
+#include <linux/hwspinlock.h>
 #include <linux/i2c/twl.h>
 #include <linux/mfd/twl6040.h>
 #include <linux/hwspinlock.h>
@@ -335,6 +336,28 @@ static struct i2c_board_info __initdata panda_i2c_eeprom[] = {
 		I2C_BOARD_INFO("eeprom", 0x50),
 	},
 };
+
+static void __init omap_i2c_hwspinlock_init(int bus_id, int spinlock_id,
+					struct omap_i2c_bus_board_data *pdata)
+{
+	/* spinlock_id should be -1 for a generic lock request */
+	if (spinlock_id < 0)
+		pdata->handle = hwspin_lock_request();
+	else
+		pdata->handle = hwspin_lock_request_specific(spinlock_id);
+
+	if (pdata->handle != NULL) {
+		pdata->hwspin_lock_timeout = hwspin_lock_timeout;
+		pdata->hwspin_unlock = hwspin_unlock;
+	} else {
+		pr_err("I2C hwspinlock request failed for bus %d\n", bus_id);
+	}
+}
+
+static struct omap_i2c_bus_board_data __initdata panda_i2c_1_bus_pdata;
+static struct omap_i2c_bus_board_data __initdata panda_i2c_2_bus_pdata;
+static struct omap_i2c_bus_board_data __initdata panda_i2c_3_bus_pdata;
+static struct omap_i2c_bus_board_data __initdata panda_i2c_4_bus_pdata;
 
 static int __init omap4_panda_i2c_init(void)
 {
