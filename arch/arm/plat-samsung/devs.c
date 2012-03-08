6a8adbf4446742be4951e0b98a311e01cbb890f6
Minsung Kim <ms925.kim@samsung.com>
2012-03-08 23:51:33 +0900

    ARM: exynos: change default i2c frequency from 100kHz to 400kHz
    Change-Id: I4259d5603aa2e792fc78821c093c5bd4b0375d83
    Signed-off-by: Minsung Kim <ms925.kim@samsung.com>

--- arch/arm/plat-samsung/devs.c
+++ arch/arm/plat-samsung/devs.c
@@ -540,7 +540,7 @@ struct platform_device s3c_device_i2c0 = {
 struct s3c2410_platform_i2c default_i2c_data __initdata = {
 	.flags		= 0,
 	.slave_addr	= 0x10,
-	.frequency	= 100*1000,
+	.frequency	= 400*1000,
 	.sda_delay	= 100,
 };
 
