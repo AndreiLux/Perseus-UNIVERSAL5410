16d51581498fadba9436a6fd3854f9ff3275549b
Rebecca Schultz Zavin <rebecca@android.com>
2012-03-08 23:14:30 -0800

    ion: exynos: Modify to reflect changes to ion api
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- drivers/gpu/ion/exynos/exynos_ion.c
+++ drivers/gpu/ion/exynos/exynos_ion.c
@@ -263,10 +263,10 @@ static void ion_exynos_heap_free(struct ion_buffer *buffer)
 	kfree(sgtable);
 }
 
-static struct scatterlist *ion_exynos_heap_map_dma(struct ion_heap *heap,
-					    struct ion_buffer *buffer)
+static struct sg_table *ion_exynos_heap_map_dma(struct ion_heap *heap,
+						struct ion_buffer *buffer)
 {
-	return ((struct sg_table *)buffer->priv_virt)->sgl;
+	return buffer->priv_virt;
 }
 
 static void ion_exynos_heap_unmap_dma(struct ion_heap *heap,
@@ -431,23 +431,27 @@ static int ion_exynos_contig_heap_phys(struct ion_heap *heap,
 	return 0;
 }
 
-static struct scatterlist *ion_exynos_contig_heap_map_dma(struct ion_heap *heap,
+static struct sg_table *ion_exynos_contig_heap_map_dma(struct ion_heap *heap,
 						   struct ion_buffer *buffer)
 {
-	struct scatterlist *sglist;
+	struct sg_table *table;
+	int ret;
 
-	sglist = vmalloc(sizeof(struct scatterlist));
-	if (!sglist)
+	table = kzalloc(sizeof(struct sg_table), GFP_KERNEL);
+	if (!table)
 		return ERR_PTR(-ENOMEM);
-	sg_init_table(sglist, 1);
-	sg_set_page(sglist, phys_to_page(buffer->priv_phys), buffer->size, 0);
-	return sglist;
+	ret = sg_alloc_table(table, 1, GFP_KERNEL);
+	if (ret)
+		return ERR_PTR(ret);
+	sg_init_one(table->sgl, phys_to_page(buffer->priv_phys), buffer->size);
+	return table;
 }
 
 static void ion_exynos_contig_heap_unmap_dma(struct ion_heap *heap,
-			       struct ion_buffer *buffer)
+					     struct ion_buffer *buffer)
 {
-	vfree(buffer->sglist);
+	if (buffer->sg_table)
+		sg_free_table(buffer->sg_table);
 }
 
 static int ion_exynos_contig_heap_map_user(struct ion_heap *heap,
