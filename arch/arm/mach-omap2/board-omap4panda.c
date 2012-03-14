e80af53504d0d9ca7ebd04ea13ea76782061cd30
Jassi Brar <jaswinder.singh@linaro.org>
2012-03-14 19:48:39 +0530

    PandaBoard: I2C: Add hwspinlock support
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -26,6 +26,7 @@
 #include <linux/usb/otg.h>
 #include <linux/i2c/twl.h>
 #include <linux/mfd/twl6040.h>
+#include <linux/hwspinlock.h>
 #include <linux/regulator/machine.h>
 #include <linux/regulator/fixed.h>
 #include <linux/wl12xx.h>
@@ -302,6 +303,29 @@ static struct twl6040_platform_data twl6040_data = {
 /* Panda board uses the common PMIC configuration */
 static struct twl4030_platform_data omap4_panda_twldata;
 
+static void __init omap_i2c_hwspinlock_init(int bus_id, int spinlock_id,
+				   struct omap_i2c_bus_board_data *pdata)
+{
+	/* spinlock_id should be -1 for a generic lock request */
+	if (spinlock_id < 0)
+		pdata->handle = hwspin_lock_request();
+	else
+		pdata->handle = hwspin_lock_request_specific(spinlock_id);
+
+	if (pdata->handle != NULL) {
+		pdata->hwspin_lock_timeout = hwspin_lock_timeout;
+		pdata->hwspin_unlock = hwspin_unlock;
+	} else {
+		pr_err("I2C hwspinlock request failed for bus %d\n", \
+									bus_id);
+	}
+}
+
+static struct omap_i2c_bus_board_data __initdata panda_i2c_1_bus_pdata;
+static struct omap_i2c_bus_board_data __initdata panda_i2c_2_bus_pdata;
+static struct omap_i2c_bus_board_data __initdata panda_i2c_3_bus_pdata;
+static struct omap_i2c_bus_board_data __initdata panda_i2c_4_bus_pdata;
+
 /*
  * Display monitor features are burnt in their EEPROM as EDID data. The EEPROM
  * is connected as I2C slave device, and can be accessed at address 0x50
@@ -314,6 +338,16 @@ static struct i2c_board_info __initdata panda_i2c_eeprom[] = {
 
 static int __init omap4_panda_i2c_init(void)
 {
+	omap_i2c_hwspinlock_init(1, 0, &panda_i2c_1_bus_pdata);
+	omap_i2c_hwspinlock_init(2, 1, &panda_i2c_2_bus_pdata);
+	omap_i2c_hwspinlock_init(3, 2, &panda_i2c_3_bus_pdata);
+	omap_i2c_hwspinlock_init(4, 3, &panda_i2c_4_bus_pdata);
+
+	omap_register_i2c_bus_board_data(1, &panda_i2c_1_bus_pdata);
+	omap_register_i2c_bus_board_data(2, &panda_i2c_2_bus_pdata);
+	omap_register_i2c_bus_board_data(3, &panda_i2c_3_bus_pdata);
+	omap_register_i2c_bus_board_data(4, &panda_i2c_4_bus_pdata);
+
 	omap4_pmic_get_config(&omap4_panda_twldata, TWL_COMMON_PDATA_USB,
 			TWL_COMMON_REGULATOR_VDAC |
 			TWL_COMMON_REGULATOR_VAUX2 |
