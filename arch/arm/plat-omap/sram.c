10282991d04e732fe1253392e58f3a57ecbe7f3d
Andy Green <andy.green@linaro.org>
2012-03-14 08:51:09 +0800

    omap5 dont map sram
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/plat-omap/sram.c
+++ arch/arm/plat-omap/sram.c
@@ -384,7 +384,9 @@ static inline int am33xx_sram_init(void)
 
 int __init omap_sram_init(void)
 {
-	omap_detect_sram();
+	if (!cpu_is_omap54xx())
+		omap_detect_sram();
+
 	omap_map_sram();
 
 	if (!(cpu_class_is_omap2()))
