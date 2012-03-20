01d9ee53ab2c4f6088ac8a2661f5ce5977e57d10
Daniel Lezcano <daniel.lezcano@linaro.org>
2012-03-19 20:15:52 -0500

    mfd : ux500 : check pending irq on gic
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- include/linux/mfd/db8500-prcmu.h
+++ include/linux/mfd/db8500-prcmu.h
@@ -538,6 +538,7 @@ void db8500_prcmu_system_reset(u16 reset_code);
 int db8500_prcmu_set_power_state(u8 state, bool keep_ulp_clk, bool keep_ap_pll);
 int db8500_prcmu_gic_decouple(void);
 int db8500_prcmu_gic_recouple(void);
+bool db8500_prcmu_gic_pending_irq(void);
 void db8500_prcmu_enable_wakeups(u32 wakeups);
 int db8500_prcmu_set_epod(u16 epod_id, u8 epod_state);
 int db8500_prcmu_request_clock(u8 clock, bool enable);
