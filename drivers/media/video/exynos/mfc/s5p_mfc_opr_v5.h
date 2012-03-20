641ddec471620af7eeb43742d40f0abf2e8bc393
Jaeryul Oh <jaeryul.oh@samsung.com>
2012-03-20 20:30:46 +0900

    media: exynos-mfc: Define minimum byte in multi-slice mode
    This patch defines minimum byte for multi-slice mode separately.
    	- MFC5.x : 238
    	- MFC6.x : 350
    
    Change-Id: Ifc0784277a3a5e1f0863b45250b3b2090a025dac
    Signed-off-by: Jeongtae Park <jtp.park@samsung.com>
    Signed-off-by: Jaeryul Oh <jaeryul.oh@samsung.com>

--- drivers/media/video/exynos/mfc/s5p_mfc_opr_v5.h
+++ drivers/media/video/exynos/mfc/s5p_mfc_opr_v5.h
@@ -123,6 +123,7 @@ void s5p_mfc_enc_calc_src_size(struct s5p_mfc_ctx *ctx);
 /* Definition */
 #define ENC_MULTI_SLICE_MB_MAX		((1 << 16) - 1)
 #define ENC_MULTI_SLICE_BIT_MIN		1900
+#define ENC_MULTI_SLICE_BYTE_MIN	238
 #define ENC_INTRA_REFRESH_MB_MAX	((1 << 16) - 1)
 #define ENC_VBV_BUF_SIZE_MAX		((1 << 16) - 1)
 #define ENC_H264_LOOP_FILTER_AB_MIN	-6
