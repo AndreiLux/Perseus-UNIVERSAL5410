b3e9130fcf6b72a566d9172a46a25e39cccee3d8
Thomas Abraham <thomas.abraham@linaro.org>
2012-03-21 07:29:24 +0530

    ARM: Exynos: Remove arch_initcall for wakeup interrupt initialization
    The of_irq_init function would be setup to invoke the exynos4_init_irq_eint
    function when booting using device tree. The arch_initcall for
    exynos4_init_irq_eint would duplicate its invocation in that case. Hence,
    arch_initcall for exynos4_init_irq_eint is removed and this function is invoked
    from the exynos4_init_irq for non-dt case.
    
    Moreover, with single kernel image build, the exynos4_init_irq_eint has no checks
    to ensure that it is running on a exynos4 platform. So it would be appropriate
    to invoke it from exynos4_init_irq.
    
    Signed-off-by: Thomas Abraham <thomas.abraham@linaro.org>
    Acked-by: Rob Herring <rob.herring@calxeda.com>

--- arch/arm/mach-exynos/common.c
+++ arch/arm/mach-exynos/common.c
@@ -64,6 +64,7 @@ static void exynos4_init_clocks(int xtal);
 static void exynos5_init_clocks(int xtal);
 static void exynos_init_uarts(struct s3c2410_uartcfg *cfg, int no);
 static int exynos_init(void);
+static int exynos_init_irq_eint(void);
 
 static struct cpu_table cpu_ids[] __initdata = {
 	{
@@ -603,6 +604,7 @@ void __init exynos4_init_irq(void)
 	 * uses GIC instead of VIC.
 	 */
 	s5p_init_irq(NULL, 0);
+	exynos_init_irq_eint();
 }
 
 void __init exynos5_init_irq(void)
@@ -615,6 +617,7 @@ void __init exynos5_init_irq(void)
 	 * uses GIC instead of VIC.
 	 */
 	s5p_init_irq(NULL, 0);
+	exynos_init_irq_eint();
 }
 
 struct bus_type exynos4_subsys = {
@@ -1044,4 +1047,3 @@ static int __init exynos_init_irq_eint(void)
 
 	return 0;
 }
-arch_initcall(exynos_init_irq_eint);
