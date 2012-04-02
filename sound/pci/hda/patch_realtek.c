84ad816c4382abdfa77e30297e8343ba707dae94
Todd Broch <tbroch@chromium.org>
2012-04-01 23:34:54 -0700

    CHROMIUM: ALSA: hda: Add fixup for alex model
    create fixup function for the alex model and override amp capabilities
    for NID 0x2 to remove clipping of audio to built-in speakers
    
    related CL(s):
    http://codereview.chromium.org/6544026/
    http://codereview.chromium.org/6469065/
    
    BUG=chrome-os-partner:2273
    TEST=manual,
    1. Build kernel and install on device
    2. Edit /etc/modprobe.d/alsa.conf to include 'options snd_hda_intel model=alex'
    3. Reboot
    4. shell>amixer -c 0 sget Speaker | grep -i limit
       Should produce 'Limits: Playback 0 - N' where N < 64 (max gain)
    
    Change-Id: Ib4efa3db95955e95421ec55bc77699d6a9faeb62
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/3140
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    
    Conflicts:
    
    	sound/pci/hda/patch_realtek.c
    [anush: fixup for 3.4]
    Signed-off-by: Anush Elangovan <anush@chromium.org>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -748,7 +748,7 @@ static void set_eapd(struct hda_codec *codec, hda_nid_t nid, int on)
 	/* delay de-assert of eapd to allow biasing of amp
 	 * inputs to settle avoiding an audible 'pop'.
 	 */
-	if (nid == 0x14) && codec->subsystem_id == 0x144dc0a7)
+	if ((nid == 0x14) && codec->subsystem_id == 0x144dc0a7)
 		msleep(25);
 
 	if (snd_hda_query_pin_caps(codec, nid) & AC_PINCAP_EAPD)
@@ -6597,6 +6597,17 @@ static void alc272_fixup_mario(struct hda_codec *codec,
 		       "hda_codec: failed to override amp caps for NID 0x2\n");
 }
 
+static void alc272_fixup_alex(struct hda_codec *codec,
+			       const struct alc_fixup *fix, int pre_init) {
+	if (snd_hda_override_amp_caps(codec, 0x2, HDA_OUTPUT,
+				      (0x3c << AC_AMPCAP_OFFSET_SHIFT) |
+				      (0x3c << AC_AMPCAP_NUM_STEPS_SHIFT) |
+				      (0x03 << AC_AMPCAP_STEP_SIZE_SHIFT) |
+				      (0 << AC_AMPCAP_MUTE_SHIFT)))
+		printk(KERN_WARNING
+		       "hda_codec: failed to override amp caps for NID 0x2\n");
+}
+
 enum {
 	ALC662_FIXUP_ASPIRE,
 	ALC662_FIXUP_IDEAPAD,
@@ -6613,6 +6624,7 @@ enum {
 	ALC662_FIXUP_ASUS_MODE7,
 	ALC662_FIXUP_ASUS_MODE8,
 	ALC662_FIXUP_NO_JACK_DETECT,
+	ALC272_FIXUP_ALEX,
 };
 
 static const struct alc_fixup alc662_fixups[] = {
@@ -6762,6 +6774,10 @@ static const struct alc_fixup alc662_fixups[] = {
 		.type = ALC_FIXUP_FUNC,
 		.v.func = alc_fixup_no_jack_detect,
 	},
+	[ALC272_FIXUP_ALEX] = {
+		.type = ALC_FIXUP_FUNC,
+		.v.func = alc272_fixup_alex,
+	}
 };
 
 static const struct snd_pci_quirk alc662_fixup_tbl[] = {
@@ -6847,6 +6863,7 @@ static const struct alc_model_fixup alc662_fixup_models[] = {
 	{.id = ALC662_FIXUP_ASUS_MODE6, .name = "asus-mode6"},
 	{.id = ALC662_FIXUP_ASUS_MODE7, .name = "asus-mode7"},
 	{.id = ALC662_FIXUP_ASUS_MODE8, .name = "asus-mode8"},
+	{.id = ALC272_FIXUP_ALEX, .name = "alex"},
 	{}
 };
 
