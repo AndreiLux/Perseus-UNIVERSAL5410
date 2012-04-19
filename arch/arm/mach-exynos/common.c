bba7253f3eec5de72d14aa613d7932f137b0b9a9
Boojin Kim <boojin.kim@samsung.com>
2012-04-20 07:44:25 +0900

    ARM: Exynos: Remove the L2 cache latency setting
    This patch removes the L2 cache latency setting because IROM code sets it
    on EXYNOS5250 EVT1.
    
    Change-Id: I963683efdd2fd92edc2024269cf507879b934b01
    Signed-off-by: Boojin Kim <boojin.kim@samsung.com>

--- arch/arm/mach-exynos/common.c
+++ arch/arm/mach-exynos/common.c
@@ -33,7 +33,6 @@
 #include <mach/regs-pmu.h>
 #include <mach/regs-gpio.h>
 #include <mach/pmu.h>
-#include <mach/smc.h>
 
 #include <plat/cpu.h>
 #include <plat/clock.h>
@@ -727,35 +726,6 @@ static int __init exynos4_l2x0_cache_init(void)
 }
 early_initcall(exynos4_l2x0_cache_init);
 #endif
-
-static int __init exynos5_l2_cache_init(void)
-{
-	unsigned int val;
-
-	if (!soc_is_exynos5250())
-		return 0;
-
-	asm volatile("mrc p15, 0, %0, c1, c0, 0\n"
-		     "bic %0, %0, #(1 << 2)\n"	/* cache disable */
-		     "mcr p15, 0, %0, c1, c0, 0\n"
-		     "mrc p15, 1, %0, c9, c0, 2\n"
-		     : "=r"(val));
-
-	val |= (1 << 9) | (1 << 5) | (2 << 6) | (2 << 0);
-
-#ifdef CONFIG_ARM_TRUSTZONE
-	exynos_smc(SMC_CMD_REG, SMC_REG_ID_CP15(9, 1, 0, 2), val, 0);
-#else
-	asm volatile("mcr p15, 1, %0, c9, c0, 2\n" : : "r"(val));
-#endif
-	asm volatile("mrc p15, 0, %0, c1, c0, 0\n"
-		     "orr %0, %0, #(1 << 2)\n"	/* cache enable */
-		     "mcr p15, 0, %0, c1, c0, 0\n"
-		     : : "r"(val));
-
-	return 0;
-}
-early_initcall(exynos5_l2_cache_init);
 
 static int __init exynos_init(void)
 {
