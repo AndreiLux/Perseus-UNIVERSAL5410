9f05c12125dcd56c1333dbc35e749e97c3b024a0
Andy Green <andy.green@linaro.org>
2012-04-20 13:07:31 +0800

    omap5 asoc board define common mux pdm
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -1259,6 +1259,26 @@ struct omap_mux_setting omap5432_common_mux[] __initdata = {
                 .name = "emmc_data7.emmc_data7",
                 .mode = OMAP_PIN_INPUT_PULLUP | OMAP_MUX_MODE0,
         },
+        {                                                                       
+                /* GPIO_112 mcpdm clk */                                        
+                .name = "abemcpdm_lb_clk.abemcpdm_lb_clk",                      
+                .mode = OMAP_PIN_INPUT | OMAP_MUX_MODE0,                        
+        },                                                                      
+        {                                                                       
+                /* GPIO_111 mcpdm frame */                                      
+                .name = "abemcpdm_frame.abemcpdm_frame",                        
+                .mode = OMAP_PIN_INPUT | OMAP_MUX_MODE0,                        
+        },                                                                      
+        {                                                                       
+                /* GPIO_110 mcpdm dl data */                                    
+                .name = "abemcpdm_dl_data.abemcpdm_dl_data",                    
+                .mode = OMAP_PIN_INPUT | OMAP_MUX_MODE0,                        
+        },                                                                      
+        {                                                                       
+                /* GPIO_109 mcpdm ul data */                                    
+                .name = "abemcpdm_ul_data.abemcpdm_ul_data",                    
+                .mode = OMAP_PIN_INPUT | OMAP_MUX_MODE0,                        
+        },
 };                                                                              
 
 /*
@@ -1304,7 +1324,7 @@ static void __init omap54xx_common_init(void)
 	platform_device_register(&leds_gpio);
 }
 
-struct omap_mux_setting omap5432_sevm_mux[] = {                                   
+struct omap_mux_setting omap5432_sevm_mux[] __initdata = {                                   
         {                                                                       
                 /* GPIO 172 - Ethernet bridge nRESET */
                 .name = "rfbi_data6.gpio6_172",
@@ -1339,7 +1359,7 @@ static void __init omap_5430_sevm_init(void)
 		pr_err("Keypad initialization failed: %d\n", status);
 }
 
-struct omap_mux_setting omap5432_uevm_mux[] = {
+struct omap_mux_setting omap5432_uevm_mux[] __initdata = {
 	{
 		/* I2C1 / PMIC scl */
 		.name = "i2c1_pmic_scl.i2c1_pmic_scl",
