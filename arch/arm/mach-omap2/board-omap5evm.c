c2aa0676310c4a8c84afa99a095da3e0e8cd8cbd
Andy Green <andy.green@linaro.org>
2012-04-20 13:05:00 +0800

    omap5 asoc board different gpio on uevm
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -1425,6 +1425,11 @@ struct omap_mux_setting omap5432_uevm_mux[] = {
 		.name = "fref_clk0_out.fref_clk0_out",
                 .mode = OMAP_PIN_OUTPUT,    
         },
+	{
+                /* GPIO_141 AUDPWRON */                       
+		.name = "mcspi1_somi.gpio5_141",
+                .mode = OMAP_PIN_OUTPUT | OMAP_MUX_MODE6,
+        },
 };
 
 
@@ -1454,6 +1459,9 @@ static void __init omap_5432_uevm_init(void)
 
 	/* WLAN module IRQ */
 	gpio_wlan_irq = 14;
+
+	/* AUDPWRON gpio */
+	twl6040_data.audpwron_gpio = 141;
 
 	omap54xx_common_init();
 }
