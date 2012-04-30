8d46783d70c304935631a8560635800f36bf5bc8
Sebastien Jan <s-jan@ti.com>
2012-04-30 16:20:51 +0800

    OMAP4460: Enable half L2 cache on 4460 ES1.0
    Enabling half of L2 cache WA only applies to 4460 ES1.0
    
    Signed-off-by: Sebastien Jan <s-jan@ti.com>
    
    Conflicts:
    
    	arch/arm/mach-omap2/omap4-common.c
    
    Conflicts:
    
    	arch/arm/mach-omap2/omap4-common.c

--- arch/arm/mach-omap2/omap4-common.c
+++ arch/arm/mach-omap2/omap4-common.c
@@ -188,19 +188,12 @@ static int __init omap_l2_cache_init(void)
 	if (omap_rev() != OMAP4430_REV_ES1_0)
 		omap_smc1(0x109, aux_ctrl);
 
-	if (cpu_is_omap446x()) {
-		writel_relaxed(0xa5a5, l2cache_base + 0x900);
-		writel_relaxed(0xa5a5, l2cache_base + 0x908);
-		writel_relaxed(0xa5a5, l2cache_base + 0x904);
-		writel_relaxed(0xa5a5, l2cache_base + 0x90C);
-	}
-
 	/*
-	 * FIXME: Temporary WA for OMAP4460 stability issue.
+	 * WA for OMAP4460 stability issue on ES1.0
 	 * Lock-down specific L2 cache ways which  makes effective
 	 * L2 size as 512 KB instead of 1 MB
 	 */
-	if (cpu_is_omap446x() && omap_rev() == OMAP4430_REV_ES1_0) {
+	if (omap_rev() == OMAP4460_REV_ES1_0) {
 		pr_info("4460 ES1.0 Cache Issue workaround enabled\n");
 		lockdown = 0xa5a5;
 		writel_relaxed(lockdown, l2cache_base + L2X0_LOCKDOWN_WAY_D0);
