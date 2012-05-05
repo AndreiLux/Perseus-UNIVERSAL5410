d2ed9c577561d228b0eb03e8c7819076e22f3ccb
Naveen Krishna Chatradhi <ch.naveen@samsung.com>
2012-05-05 23:17:02 +0530

    ARM: EXYNOS5: Add I2C2 and I2C HDMIPHY devices from device tree
    Adds I2C2 and I2C HDMIPHY devices from device tree for exynos5250
    
    Change-Id: I0d5d75c80069ccc8895f3651ce6e5377fd1f9810
    Signed-off-by: Naveen Krishna Chatradhi <ch.naveen@samsung.com>

--- arch/arm/mach-exynos/clock-exynos5.c
+++ arch/arm/mach-exynos/clock-exynos5.c
@@ -736,7 +736,6 @@ static struct clk exynos5_init_clocks_off[] = {
 		.ctrlbit	= (1 << 13),
 	}, {
 		.name		= "i2c",
-		.devname	= "s3c2440-hdmiphy-i2c",
 		.parent		= &exynos5_clk_aclk_66.clk,
 		.enable		= exynos5_clk_ip_peric_ctrl,
 		.ctrlbit	= (1 << 14),
