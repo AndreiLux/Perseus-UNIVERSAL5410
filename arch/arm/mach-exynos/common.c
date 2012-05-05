37f5c99b2e12a3d2a5e57effa721152b9ab27e2f
Padmavathi Venna <padma.v@samsung.com>
2012-05-05 12:37:36 +0530

    ARM: EXYNOS: Add static mapping for EXYNOS Audio Subsystem
    The CMU of EXYNOS can't control Audio Subsystem's clocks because
    AUDSS SFRs aren't located in CMU. But AUDSS is a kind of CMU for
    Audio Subsystem and need to use clock framework. This mapping
    address will be used for AUDSS clock control.
    
    Change-Id: If056f1e5c389f0d99748af332d27d23036e1b35a
    Signed-off-by: Padmavathi Venna <padma.v@samsung.com>

--- arch/arm/mach-exynos/common.c
+++ arch/arm/mach-exynos/common.c
@@ -195,6 +195,11 @@ static struct map_desc exynos4_iodesc[] __initdata = {
 		.pfn		= __phys_to_pfn(EXYNOS4_PA_HSPHY),
 		.length		= SZ_4K,
 		.type		= MT_DEVICE,
+	}, {
+		.virtual	= (unsigned long)S5P_VA_AUDSS,
+		.pfn		= __phys_to_pfn(EXYNOS_PA_AUDSS),
+		.length		= SZ_4K,
+		.type		= MT_DEVICE,
 	},
 };
 
@@ -281,6 +286,11 @@ static struct map_desc exynos5_iodesc[] __initdata = {
 		.virtual	= (unsigned long)S3C_VA_USB_HSPHY,
 		.pfn		= __phys_to_pfn(EXYNOS5_PA_USB_PHY),
 		.length		= SZ_256K,
+		.type		= MT_DEVICE,
+	}, {
+		.virtual	= (unsigned long)S5P_VA_AUDSS,
+		.pfn		= __phys_to_pfn(EXYNOS_PA_AUDSS),
+		.length		= SZ_4K,
 		.type		= MT_DEVICE,
 	},
 };
