2d98e39176f6670551d122bf5fd56fb7fc2b8eb0
Stéphane Marchesin <marcheu@chromium.org>
2012-05-10 16:35:12 -0700

    CHROMIUM: Fix "Work around Intel GPU bug with some physical pages."
    The previous change could have caused crashes when we fail to
    allocate the page. Instead, pass the NULL pointer back when we fail.
    
    BUG=none
    TEST=none
    
    Change-Id: I5e83370a5b20146cc3e63b880c6972d630ee1ef6
    Reviewed-on: https://gerrit.chromium.org/gerrit/22420
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Ready: Stéphane Marchesin <marcheu@chromium.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -854,7 +854,7 @@ static struct page *shmem_alloc_page(gfp_t gfp,
 	 */
 	do {
 		page = alloc_page_vma(gfp, &pvma, 0);
-	} while (!i915_usable_page(page));
+	} while (page && !i915_usable_page(page));
 
 	return page;
 }
@@ -877,7 +877,7 @@ static inline struct page *shmem_alloc_page(gfp_t gfp,
 	struct page *page;
 	do {
 		page = alloc_page(gfp);
-	} while (!i915_usable_page(page));
+	} while (page && !i915_usable_page(page));
 
 	return page;
 }
