06022bf66d1418867d6987224c05bd3a36702a99
Jon Medhurst <tixy@linaro.org>
2012-05-11 15:24:24 +0100

    gator: Default GATOR_MALI_INTERFACE_STYLE to '2'
    At the moment, we only have users with the old style interface, so
    make our life easier by making it the default.
    
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- drivers/gator/gator_events_mali.c
+++ drivers/gator/gator_events_mali.c
@@ -14,6 +14,14 @@
 
 #include "linux/mali_linux_trace.h"
 
+#if !defined(GATOR_MALI_INTERFACE_STYLE)
+/*
+ * At the moment, we only have users with the old style interface, so
+ * make our life easier by making it the default...
+ */
+#define GATOR_MALI_INTERFACE_STYLE (2)
+#endif
+
 /*
  * There are (currently) three different variants of the comms between gator and Mali:
  * 1 (deprecated): No software counter support
