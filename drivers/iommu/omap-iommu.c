2edb9badf610872043b0c433bd5e540e56f772ef
Vincent Stehlé <v-stehle@ti.com>
2012-05-17 10:51:00 +0800

    omap: iommu: fix some error returns
    Return error values instead of NULL pointers where return value is checked with
    IS_ERR(). This, to avoid kernel panics.
    
    Signed-off-by: Vincent Stehlé <v-stehle@ti.com>

--- drivers/iommu/omap-iommu.c
+++ drivers/iommu/omap-iommu.c
@@ -170,7 +170,7 @@ static inline struct cr_regs *iotlb_alloc_cr(struct omap_iommu *obj,
 					     struct iotlb_entry *e)
 {
 	if (!e)
-		return NULL;
+		return ERR_PTR(-EINVAL);
 
 	return arch_iommu->alloc_cr(obj, e);
 }
@@ -829,7 +829,7 @@ static struct omap_iommu *omap_iommu_attach(const char *name, u32 *iopgd)
 				(void *)name,
 				device_match_by_alias);
 	if (!dev)
-		return NULL;
+		return ERR_PTR(-ENODEV);
 
 	obj = to_iommu(dev);
 
