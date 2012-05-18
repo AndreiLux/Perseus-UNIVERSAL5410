AndreiLux/Perseus-UNIVERSAL5410

commit 4d33ccbffa60bbc6cf7b143b2ce4226bd553968c

dma-buf: add vmap interface

include/linux/dma-buf.h
@@ -92,6 +92,9 @@ struct dma_buf_ops {
 	void (*kunmap_atomic)(struct dma_buf *, unsigned long, void *);
 	void *(*kmap)(struct dma_buf *, unsigned long);
 	void (*kunmap)(struct dma_buf *, unsigned long, void *);
+
+	void *(*vmap)(struct dma_buf *);
+	void (*vunmap)(struct dma_buf *, void *vaddr);
 };
 
 /**
@@ -167,6 +170,9 @@ void *dma_buf_kmap_atomic(struct dma_buf *, unsigned long);
 void dma_buf_kunmap_atomic(struct dma_buf *, unsigned long, void *);
 void *dma_buf_kmap(struct dma_buf *, unsigned long);
 void dma_buf_kunmap(struct dma_buf *, unsigned long, void *);
+
+void *dma_buf_vmap(struct dma_buf *);
+void dma_buf_vunmap(struct dma_buf *, void *vaddr);
 #else
 
 static inline struct dma_buf_attachment *dma_buf_attach(struct dma_buf *dmabuf,
@@ -246,6 +252,14 @@ static inline void *dma_buf_kmap(struct dma_buf *dmabuf, unsigned long pnum)
 
 static inline void dma_buf_kunmap(struct dma_buf *dmabuf,
 				  unsigned long pnum, void *vaddr)
+{
+}
+
+static inline void *dma_buf_vmap(struct dma_buf *)
+{
+}
+
+static inline void dma_buf_vunmap(struct dma_buf *, void *vaddr);
 {
 }
 #endif /* CONFIG_DMA_SHARED_BUFFER */

drivers/base/dma-buf.c
@@ -406,3 +406,32 @@ void dma_buf_kunmap(struct dma_buf *dmabuf, unsigned long page_num,
 		dmabuf->ops->kunmap(dmabuf, page_num, vaddr);
 }
 EXPORT_SYMBOL_GPL(dma_buf_kunmap);
+
+/**
+ * dma_buf_vmap - Create virtual mapping for the buffer object into kernel address space. The same restrictions as for vmap and friends apply.
+ * @dma_buf:	[in]	buffer to vmap
+ *
+ * This call may fail due to lack of virtual mapping address space.
+ */
+void *dma_buf_vmap(struct dma_buf *dmabuf)
+{
+	WARN_ON(!dmabuf);
+
+	if (dmabuf->ops->vmap)
+		return dmabuf->ops->vmap(dmabuf);
+	return NULL;
+}
+EXPORT_SYMBOL(dma_buf_vmap);
+
+/**
+ * dma_buf_vunmap - Unmap a page obtained by dma_buf_vmap.
+ * @dma_buf:	[in]	buffer to vmap
+ */
+void dma_buf_vunmap(struct dma_buf *dmabuf, void *vaddr)
+{
+	WARN_ON(!dmabuf);
+
+	if (dmabuf->ops->vunmap)
+		dmabuf->ops->vunmap(dmabuf, vaddr);
+}
+EXPORT_SYMBOL(dma_buf_vunmap);

commit fe9bbf433976daffe728d14a1a31b49d8ff06f40

Fix videobuf2-core.c compilation error when CONFIG_DMA_SHARED_BUFFER=n
The error has been introduced by commit 4d33ccbffa60bbc6cf7b143b2ce4226bd553968c
"dma-buf: add vmap interface", which definitely was not tested for the
CONFIG_DMA_SHARED_BUFFER=n case.

Signed-off-by: Andrey Konovalov <andrey.konovalov@linaro.org>

include/linux/dma-buf.h
@@ -271,9 +271,10 @@ static inline int dma_buf_mmap(struct dma_buf *dmabuf,
 
 static inline void *dma_buf_vmap(struct dma_buf *dmabuf)
 {
+	return NULL;
 }
 
-static inline void dma_buf_vunmap(struct dma_buf *dmabuf, void *vaddr);
+static inline void dma_buf_vunmap(struct dma_buf *dmabuf, void *vaddr)
 {
 }
 #endif /* CONFIG_DMA_SHARED_BUFFER */