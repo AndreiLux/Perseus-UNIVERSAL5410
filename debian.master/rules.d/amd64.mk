5399f7c26ffa2a86fca6ff29c95156cc018a9808
John Rigby <john.rigby@linaro.org>
2012-05-20 23:42:40 -0600

    Linaro: Squashed Ubuntu-3.4.0-2.6 packaging for upstream v3.4-rc7
    Signed-off-by: John Rigby <john.rigby@linaro.org>

---
+++ debian.master/rules.d/amd64.mk
@@ -0,0 +1,10 @@
+human_arch	= 64 bit x86
+build_arch	= x86_64
+header_arch	= $(build_arch)
+defconfig	= defconfig
+flavours	= generic virtual
+build_image	= bzImage
+kernel_file	= arch/$(build_arch)/boot/bzImage
+install_file	= vmlinuz
+loader		= grub
+no_dumpfile	= true
