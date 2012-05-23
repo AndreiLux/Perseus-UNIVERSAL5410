4020f46bc922a2e4fdd4de0baa0eb79737310e7b
Seungwon Jeon <tgih.jun@samsung.com>
2012-05-23 10:55:54 -0700

    mmc: dw_mmc: correct the calculation for CLKDIV
    In case of "host->bus_hz < slot->clock", divider value is
    miscalculated. And clock divider register value is multiple
    of 2. If calculated divider value is odd number, result can
    be over-clocking.
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -635,15 +635,16 @@ static void dw_mci_setup_bus(struct dw_mci_slot *slot, int force)
 	u32 div;
 
 	if ((slot->clock != host->current_speed) || force) {
+		div = host->bus_hz / slot->clock;
 		if ((host->bus_hz % slot->clock) &&
 			(host->bus_hz > slot->clock))
 			/*
 			 * move the + 1 after the divide to prevent
 			 * over-clocking the card.
 			 */
-			div = ((host->bus_hz / slot->clock) >> 1) + 1;
-		else
-			div = (host->bus_hz  / slot->clock) >> 1;
+			div++;
+
+		div = (host->bus_hz != slot->clock) ? DIV_ROUND_UP(div, 2) : 0;
 
 		dev_info(&slot->mmc->class_dev,
 			 "Bus speed (slot %d) = %dHz (slot req %dHz, actual %dHZ"
