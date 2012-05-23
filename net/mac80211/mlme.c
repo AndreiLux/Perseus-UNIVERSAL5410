e277e49b6aabd4a07b5b79201be50b2de9198eee
Gary Morain <gmorain@chromium.org>
2012-05-23 01:23:48 +0000

    CHROMIUMOS: mac80211: Increase retry attempts for nullfunc probes
    When a nullfunc probe fails for lack of an ACK, it is retried
    almost immediately.  The current setting of 10 retries completes
    in about 20 ms.  If an AP is off-channel scanning, this is not
    enough time for the AP to return to the operating channel and
    ACK the nullfunc.  As a result, the client disconnects when the
    AP is still there and operating.
    
    BUG=chrome-os-partner:8694
    TEST=manual.  Let run overnight to verify that 100 is a sufficient
    number of retries.  50 is too low.
    
    Change-Id: I8c87779c42b04489bec08b9b376e916e39ce5a4e
    Signed-off-by: Gary Morain <gmorain@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/23063
    Reviewed-by: Paul Stewart <pstew@chromium.org>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -36,7 +36,7 @@
 #define IEEE80211_ASSOC_TIMEOUT (HZ / 5)
 #define IEEE80211_ASSOC_MAX_TRIES 3
 
-static int max_nullfunc_tries = 10;
+static int max_nullfunc_tries = 100;
 module_param(max_nullfunc_tries, int, 0644);
 MODULE_PARM_DESC(max_nullfunc_tries,
 		 "Maximum nullfunc tx tries before disconnecting (reason 4).");
