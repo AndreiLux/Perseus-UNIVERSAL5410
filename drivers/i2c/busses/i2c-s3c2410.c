AndreiLux/Perseus-UNIVERSAL5410

commit 6b4543d58d6618cb3ffa2383781a2c0d666945db

i2c-s3c2410: add check routine for uninitialized i2c transfer
If i2c is suspended status.
Clock enable is unnecessarily and transfer is wrong use case.
So, Check it and print error message.

Change-Id: Id3d4175c5f4c33fac0079dd9a360f2650d256221
Signed-off-by: Mingyu Lee <coollmg.lee@samsung.com>

drivers/i2c/busses/i2c-s3c2410.c
@@ -1,4 +1,4 @@
-/* linux/drivers/i2c/busses/i2c-s3c2410.c
+z/* linux/drivers/i2c/busses/i2c-s3c2410.c
  *
  * Copyright (C) 2004,2005,2009 Simtec Electronics
  *	Ben Dooks <ben@simtec.co.uk>
@@ -614,6 +614,11 @@ static int s3c24xx_i2c_xfer(struct i2c_adapter *adap,
 	struct s3c24xx_i2c *i2c = (struct s3c24xx_i2c *)adap->algo_data;
 	int retry;
 	int ret;
+
+	if (i2c->suspended) {
+		dev_err(i2c->dev, "I2C is not initialzed.\n");
+		return -EREMOTEIO;
+	}
 
 	pm_runtime_get_sync(&adap->dev);
 	clk_enable(i2c->clk);

commit 9a27c3ff8e4a234af453d8a6817f4b7fc3a62827

i2c: s3c2410: fix build error
A cherry-pick of 6b4543d5 introduced a typo, fix it.

Change-Id: I7c419f03762fc12e5f677c66e0f41b3149869d25
Signed-off-by: Colin Cross <ccross@android.com>

drivers/i2c/busses/i2c-s3c2410.c
@@ -1,4 +1,4 @@
-z/* linux/drivers/i2c/busses/i2c-s3c2410.c
+/* linux/drivers/i2c/busses/i2c-s3c2410.c
  *
  * Copyright (C) 2004,2005,2009 Simtec Electronics
  *	Ben Dooks <ben@simtec.co.uk>