64a01023c0bb2900aee63b33d58b5fc8257021f4
Kyoungil Kim <ki0351.kim@samsung.com>
2012-05-30 17:30:14 +0900

    serial: samsung: protect NULL dereference of clock name
    When priting the serial clock source, if clock source name is null,
    kernel reference NULL point.
    
    Change-Id: If0c9cf560c3b119eebd62db169749a4bc0027147
    Signed-off-by: KeyYoung Park <keyyoung.park@samsung.com>
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>
    Signed-off-by: Kyoungil Kim <ki0351.kim@samsung.com>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -1160,7 +1160,8 @@ static ssize_t s3c24xx_serial_show_clksrc(struct device *dev,
 	struct uart_port *port = s3c24xx_dev_to_port(dev);
 	struct s3c24xx_uart_port *ourport = to_ourport(port);
 
-	return snprintf(buf, PAGE_SIZE, "* %s\n", ourport->baudclk->name);
+	return snprintf(buf, PAGE_SIZE, "* %s\n",
+			ourport->baudclk->name ?: "(null)");
 }
 
 static DEVICE_ATTR(clock_source, S_IRUGO, s3c24xx_serial_show_clksrc, NULL);
