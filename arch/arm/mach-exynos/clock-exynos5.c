93a6be130f1d9e44fec8d8ef4ac1520be4abad64
Jonghwan Choi <jhbird.choi@samsung.com>
2012-06-01 14:02:45 +0900

    ARM: EXYNOS: Add clock gating feature for G3D
    This patch adds support for G3D clock gating.
    
    Change-Id: Id1cdccd7d920036b2cb1754726aa4a7dc4c0fe14
    Signed-off-by: Jonghwan Choi <jhbird.choi@samsung.com>

--- arch/arm/mach-exynos/clock-exynos5.c
+++ arch/arm/mach-exynos/clock-exynos5.c
@@ -268,6 +268,11 @@ static int exynos5_clk_ip_mfc_ctrl(struct clk *clk, int enable)
 	return s5p_gatectrl(EXYNOS5_CLKGATE_IP_MFC, clk, enable);
 }
 
+static int exynos5_clk_ip_g3d_ctrl(struct clk *clk, int enable)
+{
+	return s5p_gatectrl(EXYNOS5_CLKGATE_IP_G3D, clk, enable);
+}
+
 static int exynos5_clk_ip_peric_ctrl(struct clk *clk, int enable)
 {
 	return s5p_gatectrl(EXYNOS5_CLKGATE_IP_PERIC, clk, enable);
@@ -1004,6 +1009,11 @@ static struct clk exynos5_init_clocks_off[] = {
 		.devname	= "s5p-mfc",
 		.enable		= exynos5_clk_ip_mfc_ctrl,
 		.ctrlbit	= ((1 << 4) | (1 << 3) | (1 << 0)),
+	}, {
+		.name		= "g3d",
+		.devname	= "mali.0",
+		.enable		= exynos5_clk_ip_g3d_ctrl,
+		.ctrlbit	= ((1 << 1) | (1 << 0)),
 	}, {
 		.name		= "isp0",
 		.devname	= FIMC_IS_MODULE_NAME,
@@ -1845,7 +1855,7 @@ static struct clksrc_clk exynos5_clksrcs[] = {
 	}, {
 		.clk	= {
 			.name		= "sclk_g3d",
-			.devname	= "mali-t604.0",
+			.devname	= "mali.0",
 			.enable		= exynos5_clk_block_ctrl,
 			.ctrlbit	= (1 << 1),
 		},
