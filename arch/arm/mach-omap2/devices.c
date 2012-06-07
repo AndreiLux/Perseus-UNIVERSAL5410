2e0c2ad90afe6607bf2099bd6f62235af7cba3be
Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>
2012-06-07 17:07:51 +0800

    omap: devices: fix compilation error if CONFIG_IOMMU_API is undefined
    Fix the following compilation error:
    arch/arm/mach-omap2/devices.c: In function 'omap2_init_devices':
    arch/arm/mach-omap2/devices.c:756: error: 'bogus_isp_pdata' undeclared (first use in this function)
    arch/arm/mach-omap2/devices.c:756: error: (Each undeclared identifier is reported only once
    arch/arm/mach-omap2/devices.c:756: error: for each function it appears in.)
    make[1]: *** [arch/arm/mach-omap2/devices.o] Error 1
    
    Signed-off-by: Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>
    Signed-off-by: Fernando Guzman Lugo <fernando.lugo@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -21,6 +21,7 @@
 #include <linux/netdevice.h>
 #include <linux/if_ether.h>
 #include <linux/pm_runtime.h>
+#include <media/omap3isp.h>
 
 #include <mach/hardware.h>
 #include <mach/irqs.h>
@@ -144,6 +145,8 @@ static struct platform_device omap2cam_device = {
 };
 #endif
 
+static struct isp_platform_data bogus_isp_pdata;
+
 #if defined(CONFIG_IOMMU_API)
 
 #include <plat/iommu.h>
@@ -235,12 +238,6 @@ static struct platform_device omap3isp_device = {
 static struct omap_iommu_arch_data omap3_isp_iommu = {
 	.name = "isp",
 };
-
-struct isp_platform_data {
-	struct isp_v4l2_subdevs_group *subdevs;
-	void (*set_constraints)(struct isp_device *isp, bool enable);
-};
-static struct isp_platform_data bogus_isp_pdata;
 
 int omap3_init_camera(struct isp_platform_data *pdata)
 {
