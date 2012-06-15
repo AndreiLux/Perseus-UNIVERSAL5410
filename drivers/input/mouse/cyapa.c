77121e71d593c7344e9d9e8ca4cdd91631893fdc
Benson Leung <bleung@chromium.org>
2012-06-14 20:21:27 -0700

    CHROMIUM: Input: cyapa - Fix copyright and description
    BUG=chromium-os:21047
    TEST=Builds clean.
    
    Change-Id: Ic41eb8d3d02cb150ebcbe9e53091de3d80bbb302
    Signed-off-by: Benson Leung <bleung@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/25464
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- drivers/input/mouse/cyapa.c
+++ drivers/input/mouse/cyapa.c
@@ -3,11 +3,8 @@
  *
  * Author: Dudley Du <dudl@cypress.com>
  *
- * Copyright (C) 2011 Cypress Semiconductor, Inc.
- * Copyright (C) 2011 Google, Inc.
- *
- * Based on synaptics_i2c driver:
- *   Copyright (C) 2009 Compulab, Ltd.
+ * Copyright (C) 2011-2012 Cypress Semiconductor, Inc.
+ * Copyright (C) 2011-2012 Google, Inc.
  *
  * This file is subject to the terms and conditions of the GNU General Public
  * License.  See the file COPYING in the main directory of this archive for
