a14519ae9eecdfdd0685442942a24ddb71cf6ed1
Roger Quadros <rogerq@ti.com>
2012-06-22 09:03:25 +0800

    OMAP: Smartreflex: Don't break suspend/resume
    This patch disables Smartreflex voltage autocompensation
    (if it was enabled) while suspending and enables it back
    during resume.
    
    Now system is able to suspend and resume correctly.
    
    Signed-off-by: Roger Quadros <rogerq@ti.com>

--- arch/arm/mach-omap2/smartreflex.c
+++ arch/arm/mach-omap2/smartreflex.c
@@ -45,7 +45,8 @@ struct omap_sr {
 	int				srid;
 	int				ip_type;
 	int				nvalue_count;
-	bool				autocomp_active;
+	bool				autocomp_active; /* current state  */
+	bool				autocomp_state;  /*for suspend/resume*/
 	u32				clk_length;
 	u32				err_weight;
 	u32				err_minlimit;
@@ -1119,11 +1120,37 @@ static void __devexit omap_sr_shutdown(struct platform_device *pdev)
 	return;
 }
 
+static int sr_suspend(struct device *dev)
+{
+	struct omap_sr *sr_info = dev_get_drvdata(dev);
+
+	/* save autocomp state and disable autocomp */
+	sr_info->autocomp_state = sr_info->autocomp_active;
+	if (sr_info->autocomp_active)
+		sr_stop_vddautocomp(sr_info, true, 1);
+
+	return 0;
+}
+
+static int sr_resume(struct device *dev)
+{
+	struct omap_sr *sr_info = dev_get_drvdata(dev);
+
+	/* enable autocomp if it was enabled before suspend */
+	if (sr_info->autocomp_state)
+		sr_start_vddautocomp(sr_info, true);
+
+	return 0;
+}
+
+static SIMPLE_DEV_PM_OPS(sr_pm_ops, sr_suspend, sr_resume);
+
 static struct platform_driver smartreflex_driver = {
 	.remove         = __devexit_p(omap_sr_remove),
 	.shutdown	= __devexit_p(omap_sr_shutdown),
 	.driver		= {
 		.name	= "smartreflex",
+		.pm	= &sr_pm_ops,
 	},
 };
 
