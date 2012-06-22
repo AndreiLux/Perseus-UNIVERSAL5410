a4e0e1f86db91be424dd38710c2763122b012f9e
Olof Johansson <olofj@chromium.org>
2012-06-21 18:08:08 -0700

    CHROMIUM: cpu_callback can't be __cpuinit
    This is because lockup_detector_bootcpu_resume() now uses it, so
    it's used after runtime init of cpus.
    
    BUG=none
    TEST=build with CONFIG_DEBUG_SECTION_MISMATCH=y, watch the warnings go away
    
    Change-Id: I1506e351b9ddeae699245a31f4e45805ac2fd52e
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/25898
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -546,7 +546,7 @@ out:
 /*
  * Create/destroy watchdog threads as CPUs come and go:
  */
-static int __cpuinit
+static int
 cpu_callback(struct notifier_block *nfb, unsigned long action, void *hcpu)
 {
 	int hotcpu = (unsigned long)hcpu;
@@ -581,7 +581,7 @@ cpu_callback(struct notifier_block *nfb, unsigned long action, void *hcpu)
 	return NOTIFY_OK;
 }
 
-static struct notifier_block __cpuinitdata cpu_nfb = {
+static struct notifier_block cpu_nfb = {
 	.notifier_call = cpu_callback
 };
 
