a048428d686f0f52b43b291b5abdfe3ea42732c8
Andy Green <andy.green@linaro.org>
2012-06-26 21:58:24 +0800

    clean remove warning from omap_vout
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- drivers/media/video/omap/omap_vout.c
+++ drivers/media/video/omap/omap_vout.c
@@ -328,7 +328,7 @@ static int video_mode_to_dss_mode(struct omap_vout_device *vout)
 	struct omap_overlay *ovl;
 	struct omapvideo_info *ovid;
 	struct v4l2_pix_format *pix = &vout->pix;
-	enum omap_color_mode mode;
+	enum omap_color_mode mode = -EINVAL;
 
 	ovid = &vout->vid_info;
 	ovl = ovid->overlays[0];
