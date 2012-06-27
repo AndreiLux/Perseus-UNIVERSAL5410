ba2953b11a8433618cdbd127bb01d0fe06aa5214
Kevin Hilman <khilman@ti.com>
2012-06-26 18:45:32 -0700

    I2C: OMAP: xfer: fix runtime PM get/put balance on error
    In omap_i2c_xfer(), ensure pm_runtime_put() is called, even on
    failure.
    
    Without this, after a failed xfer, the runtime PM usecount will have
    been incremented, but not decremented causing the usecount to never
    reach zero after a failure.  This keeps the device always runtime PM
    enabled which keeps the enclosing power domain active, and prevents
    full-chip retention/off from happening during idle.
    
    Signed-off-by: Kevin Hilman <khilman@ti.com>
    Reviewed-by: Shubhrajyoti D <shubhrajyoti@ti.com>
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>

--- drivers/i2c/busses/i2c-omap.c
+++ drivers/i2c/busses/i2c-omap.c
@@ -666,7 +666,7 @@ omap_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 
 	r = pm_runtime_get_sync(dev->dev);
 	if (IS_ERR_VALUE(r))
-		return r;
+		goto out;
 
 #if USE_HW_SPINLOCK
 	r = omap_i2c_hwspinlock_lock(dev);
