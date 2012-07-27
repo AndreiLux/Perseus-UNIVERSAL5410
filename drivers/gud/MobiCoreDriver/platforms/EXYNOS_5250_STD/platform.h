f5641a2debed1f2a6c976821d69f21c4c9a70dd9
DongJin Park <djpax.park@samsung.com>
2012-07-27 12:13:06 +0900

    mobicore: Initial push of MobiCore Driver
    This commit adds mobicore driver at Linux 3.4 kernel.
    To use mobicore driver, the followings should be prepared:
    - To install MobiCore at TZSW partition
    - To define CONFIG_ARM_TRUSTZONE
    Mobicore driver exists under "driver/gud".
    
    This driver comes from SAMSUNG_Exynos5250_EngineeringRelease_MC1.2_V002.
    
    Change-Id: Ie1b9bb179f051716cfe5649ec20776c3421c1f50
    Signed-off-by: DongJin Park <djpax.park@samsung.com>

---
+++ drivers/gud/MobiCoreDriver/platforms/EXYNOS_5250_STD/platform.h
@@ -0,0 +1,29 @@
+/**
+ * Header file of MobiCore Driver Kernel Module Platform
+ * specific structures
+ *
+ * @addtogroup MobiCore_Driver_Kernel_Module
+ * @{
+ * Internal structures of the McDrvModule
+ * @file
+ *
+ * Header file the MobiCore Driver Kernel Module,
+ * its internal structures and defines.
+ *
+ * <!-- Copyright Giesecke & Devrient GmbH 2009-2012 -->
+ */
+
+#ifndef _MC_DRV_PLATFORM_H_
+#define _MC_DRV_PLATFORM_H_
+
+/** MobiCore Interrupt. */
+#define MC_INTR_SSIQ                        (154)
+
+/** Enable mobicore mem traces */
+#define MC_MEM_TRACES
+
+/** Change to Android UID check */
+#define MC_ANDROID_UID_CHECK
+
+#endif /* _MC_DRV_PLATFORM_H_ */
+/** @} */
