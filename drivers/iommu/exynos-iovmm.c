ab620c07daaa015785dc10dc88c50548fc0698e7
Dima Zavin <dima@android.com>
2012-07-30 14:48:07 -0700

    iommu: exynos: unmap regions before freeing them back to genpool
    Previously, the iovmm driver was freeing the data back to the
    genpool, which would have allowed it to get reused on the next
    map call by the device concurrently with it being unmapped.
    
    Change-Id: I8e10f5b851b418c93e3b472a463270d3584e7583
    Signed-off-by: Dima Zavin <dima@android.com>

--- drivers/iommu/exynos-iovmm.c
+++ drivers/iommu/exynos-iovmm.c
@@ -199,11 +199,11 @@ void iovmm_unmap(struct device *dev, dma_addr_t iova)
 
 	region->start = round_down(region->start, PAGE_SIZE);
 
-	gen_pool_free(vmm->vmm_pool, region->start, region->size);
-
 	unmapped_size = iommu_unmap(vmm->domain, region->start, region->size);
 
 	exynos_sysmmu_tlb_invalidate(dev);
+
+	gen_pool_free(vmm->vmm_pool, region->start, region->size);
 
 	WARN_ON(unmapped_size != region->size);
 	dev_dbg(dev, "IOVMM: Unmapped %#x bytes from %#x.\n",
