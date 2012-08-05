a95ada2c79dcf92f483c3110b4c6dc013d86045a
Jassi Brar <jaswinder.singh@linaro.org>
2012-08-05 23:37:57 +0530

    OMAP: Remove duplicate mcpdm init
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -1172,7 +1172,6 @@ static int __init omap2_init_devices(void)
 	omap_init_aess();
 	omap_init_audio();
 	omap_init_mcasp();
-	omap_init_mcpdm();
 	omap_init_dmic();
 	omap_init_mcpdm();
 	omap_init_camera();
