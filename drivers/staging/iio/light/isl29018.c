4e2c7661ca8f6ec6422e09d5ebb5a28708d9267a
Bryan Freed <bfreed@chromium.org>
2012-08-28 21:10:25 -0700

    CHROMIUM: iio: isl29018: Support fractional ALS scaling.
    The Industrial IO framework supports scaling ADC values by fractions,
    but most drivers default to using whole numbers.
    This change turns on fractional scaling in the isl29018 driver.
    
    BUG=chrome-os-partner:11948
    TEST='echo 1.5 > in_illuminance_calibscale', and verify it has the
         expected effect on in_illuminance0_input.
    
    Change-Id: Ie8e2e55a2a2fe008de513abd955615f5d16204ce
    Signed-off-by: Bryan Freed <bfreed@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/31683
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- drivers/staging/iio/light/isl29018.c
+++ drivers/staging/iio/light/isl29018.c
@@ -64,6 +64,7 @@ struct isl29018_chip {
 	struct i2c_client	*client;
 	struct mutex		lock;
 	unsigned int		lux_scale;
+	unsigned int		lux_uscale;
 	unsigned int		range;
 	unsigned int		adc_bit;
 	int			prox_scheme;
@@ -182,6 +183,7 @@ static int isl29018_read_lux(struct i2c_client *client, int *lux)
 {
 	int lux_data;
 	struct isl29018_chip *chip = iio_priv(i2c_get_clientdata(client));
+	unsigned int data_x_range, lux_unshifted;
 
 	lux_data = isl29018_read_sensor_input(client,
 				COMMMAND1_OPMODE_ALS_ONCE);
@@ -189,7 +191,15 @@ static int isl29018_read_lux(struct i2c_client *client, int *lux)
 	if (lux_data < 0)
 		return lux_data;
 
-	*lux = (lux_data * chip->range * chip->lux_scale) >> chip->adc_bit;
+	/* To support fractional scaling, separate the unshifted lux
+	 * into two calculations: int scaling and micro-scaling.
+	 * lux_uscale ranges from 0-999999, so about 20 bits.  Split
+	 * the /1,000,000 in two to reduce the risk of over/underflow.
+	 */
+	data_x_range = lux_data * chip->range;
+	lux_unshifted = data_x_range * chip->lux_scale;
+	lux_unshifted += data_x_range / 1000 * chip->lux_uscale / 1000;
+	*lux = lux_unshifted >> chip->adc_bit;
 
 	return 0;
 }
@@ -468,6 +478,8 @@ static int isl29018_write_raw(struct iio_dev *indio_dev,
 	mutex_lock(&chip->lock);
 	if (mask == IIO_CHAN_INFO_CALIBSCALE && chan->type == IIO_LIGHT) {
 		chip->lux_scale = val;
+		/* With no write_raw_get_fmt(), val2 is a MICRO fraction. */
+		chip->lux_uscale = val2;
 		ret = 0;
 	}
 	mutex_unlock(&chip->lock);
@@ -508,7 +520,8 @@ static int isl29018_read_raw(struct iio_dev *indio_dev,
 	case IIO_CHAN_INFO_CALIBSCALE:
 		if (chan->type == IIO_LIGHT) {
 			*val = chip->lux_scale;
-			ret = IIO_VAL_INT;
+			*val2 = chip->lux_uscale;
+			ret = IIO_VAL_INT_PLUS_MICRO;
 		}
 		break;
 	default:
