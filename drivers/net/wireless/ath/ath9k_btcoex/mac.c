6899e4bb666cd7558d0f01473c5ffbb3a28e4234
Felix Fietkau <nbd@openwrt.org>
2012-09-07 23:25:24 +0530

    CHROMIUM: ath9k: fix interrupt storms on queued hardware reset
    commit b74713d04effbacd3d126ce94cec18742187b6ce
    "ath9k: Handle fatal interrupts properly" introduced a race condition,
    where
    IRQs are being left enabled, however the irq handler returns IRQ_HANDLED
    while the reset is still queued without addressing the IRQ cause.
    This leads to an IRQ storm that prevents the system from even getting to
    the reset code.
    
    Fix this by disabling IRQs in the handler without touching intr_ref_cnt.
    
    BUG=chrome-os-partner:12560
    TEST=compile
    
    Change-Id: I10a6c530dc3dbda66ed5f54e09251f6f109d43c2
    Cc: Rajkumar Manoharan <rmanohar@qca.qualcomm.com>
    Cc: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: mukesh agrawal <quiche@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/32619

--- drivers/net/wireless/ath/ath9k_btcoex/mac.c
+++ drivers/net/wireless/ath/ath9k_btcoex/mac.c
@@ -765,14 +765,9 @@ bool ath9k_btcoex_ath9k_hw_intrpend(struct ath_hw *ah)
 }
 EXPORT_SYMBOL(ath9k_btcoex_ath9k_hw_intrpend);
 
-void ath9k_btcoex_ath9k_hw_disable_interrupts(struct ath_hw *ah)
+void ath9k_btcoex_ath9k_hw_kill_interrupts(struct ath_hw *ah)
 {
 	struct ath_common *common = ath9k_hw_common(ah);
-
-	if (!(ah->imask & ATH9K_INT_GLOBAL))
-		atomic_set(&ah->intr_ref_cnt, -1);
-	else
-		atomic_dec(&ah->intr_ref_cnt);
 
 	ath_dbg(common, INTERRUPT, "disable IER\n");
 	REG_WRITE(ah, AR_IER, AR_IER_DISABLE);
@@ -785,6 +780,18 @@ void ath9k_btcoex_ath9k_hw_disable_interrupts(struct ath_hw *ah)
 		(void) REG_READ(ah, AR_INTR_SYNC_ENABLE);
 	}
 }
+EXPORT_SYMBOL(ath9k_btcoex_ath9k_hw_kill_interrupts);
+
+void ath9k_btcoex_ath9k_hw_disable_interrupts(struct ath_hw *ah)
+{
+	if (!(ah->imask & ATH9K_INT_GLOBAL))
+		atomic_set(&ah->intr_ref_cnt, -1);
+	else
+		atomic_dec(&ah->intr_ref_cnt);
+
+	ath9k_btcoex_ath9k_hw_kill_interrupts(ah);
+}
+
 EXPORT_SYMBOL(ath9k_btcoex_ath9k_hw_disable_interrupts);
 
 void ath9k_btcoex_ath9k_hw_enable_interrupts(struct ath_hw *ah)
