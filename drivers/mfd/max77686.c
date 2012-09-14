07cfaa3927473f6e6dae791a42348376ddb9b5ed
Jonathan Kliegman <kliegs@chromium.org>
2012-09-14 15:12:17 -0400

    mfd: max77686: Fix section mismatch warning
    max77686_i2c_probe references devinit data but isn't marked __devinit.
    As this is a device init function called via i2c_add_driver callbacks,
    mark max77686_i2c_probe as __devinit
    
    BUG=None
    TEST=Warnings removed, no obvious regressions on device
    
    Change-Id: I17bb2bc4559eb3d343808a7606f1158e7f112b3c
    Signed-off-by: Jonathan Kliegman <kliegs@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/33352
    Reviewed-by: Doug Anderson <dianders@chromium.org>

--- drivers/mfd/max77686.c
+++ drivers/mfd/max77686.c
@@ -161,8 +161,8 @@ static inline int max77686_i2c_get_driver_data(struct i2c_client *i2c,
 	return (int)id->driver_data;
 }
 
-static int max77686_i2c_probe(struct i2c_client *i2c,
-			      const struct i2c_device_id *id)
+static int __devinit max77686_i2c_probe(struct i2c_client *i2c,
+					const struct i2c_device_id *id)
 {
 	struct max77686_dev *max77686;
 	struct max77686_platform_data *pdata = i2c->dev.platform_data;
