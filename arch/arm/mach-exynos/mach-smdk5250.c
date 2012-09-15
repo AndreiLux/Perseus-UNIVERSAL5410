8b3a302687a89a93b14b684a2a8429a24febb6ea
Colin Cross <ccross@android.com>
2012-09-15 10:27:01 -0700

    Revert "ARM: EXYNOS: smdk5250: add threshold temperature for MIF voltage compensation."
    This reverts commit c2db38c54242b1f84034a5a12155a5de733fc739.

--- arch/arm/mach-exynos/mach-smdk5250.c
+++ arch/arm/mach-exynos/mach-smdk5250.c
@@ -1550,8 +1550,6 @@ static struct platform_device *smdk5250_devices[] __initdata = {
 /* TMU */
 static struct tmu_data smdk5250_tmu_pdata __initdata = {
 	.ts = {
-		.stop_mif_vc		= 27,
-		.start_mif_vc		= 25,
 		.stop_throttle		= 78,
 		.start_throttle		= 80,
 		.start_tripping		= 110,
