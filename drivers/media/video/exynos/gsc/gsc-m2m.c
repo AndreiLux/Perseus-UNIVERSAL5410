0e0024fdaf41747b34b42880086a6745f188eb08
Shaik Ameer Basha <shaik.ameer@samsung.com>
2012-09-17 17:01:40 +0530

    media: gsc: variable type change for proper error handling
    Changing the "ret" variable type to "int". Previous type was "u32",
    which fails to store the negative error values.
    With this patch we can handle the error sequences properly.
    
    BUG=chrome-os-partner:14132
    
    Change-Id: I8355b25d693593e769a400a4d9f13b4463921b43
    Signed-off-by: Shaik Ameer Basha <shaik.ameer@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/33410
    Reviewed-by: Leela Krishna Amudala <l.krishna@samsung.com>
    Reviewed-by: Pawel Osciak <posciak@google.com>
    Commit-Ready: Alim Akhtar <alim.akhtar@samsung.com>
    Reviewed-by: Alim Akhtar <alim.akhtar@samsung.com>

--- drivers/media/video/exynos/gsc/gsc-m2m.c
+++ drivers/media/video/exynos/gsc/gsc-m2m.c
@@ -119,7 +119,7 @@ static void gsc_m2m_device_run(void *priv)
 	struct gsc_ctx *ctx = priv;
 	struct gsc_dev *gsc;
 	unsigned long flags;
-	u32 ret;
+	int ret = 0;
 	bool is_set = false;
 
 	if (WARN(!ctx, "null hardware context\n"))
