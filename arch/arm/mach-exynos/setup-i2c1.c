e433379327547b4ce54a879dc446e59ffcd8b978
Sachin Kamat <sachin.kamat@linaro.org>
2012-10-16 15:29:29 +0530

    ARM: EXYNOS: Update I2C 1 and 3 settings as per Insignal code
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>

--- arch/arm/mach-exynos/setup-i2c1.c
+++ arch/arm/mach-exynos/setup-i2c1.c
@@ -19,5 +19,6 @@ struct platform_device; /* don't need the contents */
 void s3c_i2c1_cfg_gpio(struct platform_device *dev)
 {
 	s3c_gpio_cfgall_range(EXYNOS4_GPD1(2), 2,
-			      S3C_GPIO_SFN(2), S3C_GPIO_PULL_UP);
+			S3C_GPIO_SFN(2), S3C_GPIO_PULL_NONE);
+
 }
--- arch/arm/mach-exynos/setup-i2c3.c
+++ arch/arm/mach-exynos/setup-i2c3.c
@@ -20,4 +20,7 @@ void s3c_i2c3_cfg_gpio(struct platform_device *dev)
 {
 	s3c_gpio_cfgall_range(EXYNOS4_GPA1(2), 2,
 			      S3C_GPIO_SFN(3), S3C_GPIO_PULL_UP);
+
+	 s5p_gpio_set_drvstr(EXYNOS4_GPA1(2), 3);
+	 s5p_gpio_set_drvstr(EXYNOS4_GPA1(3), 3);
 }
