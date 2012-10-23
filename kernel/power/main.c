6450dd697105fe862d7fd823d2220a076241cafc
Andrei F <luxneb@gmail.com>
2012-10-24 00:10:10 +0200

    power: neuter the cpufreq min samsung interface again

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -515,11 +515,11 @@ static ssize_t cpufreq_min_limit_store(struct kobject *kobj,
 	} else { /* Lock request */
 		if (get_cpufreq_level((unsigned int)val, &cpufreq_level)
 			== VALID_LEVEL) {
-			if (cpufreq_min_limit_val != -1)
+			//if (cpufreq_min_limit_val != -1)
 				/* Unlock the previous lock */
-				exynos_cpufreq_lock_free(DVFS_LOCK_ID_USER);
-			lock_ret = exynos_cpufreq_lock(
-					DVFS_LOCK_ID_USER, cpufreq_level);
+			//	exynos_cpufreq_lock_free(DVFS_LOCK_ID_USER);
+			//lock_ret = exynos_cpufreq_lock(
+			//		DVFS_LOCK_ID_USER, cpufreq_level);
 			/* ret of exynos_cpufreq_lock is meaningless.
 			   0 is fail? success? */
 			cpufreq_min_limit_val = val;
