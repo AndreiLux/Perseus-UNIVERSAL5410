3f1dff11ac95eda2772bef577e368bc124bfe087
Morten Rasmussen <morten.rasmussen@arm.com>
2012-11-16 18:32:40 +0000

    ARM: TC2: Re-enable SD_SHARE_POWERLINE
    Re-enable SD_SHARE_POWERLINE to reflect the power domains of TC2.

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -228,7 +228,7 @@ struct cputopo_arm cpu_topology[NR_CPUS];
 
 int arch_sd_share_power_line(void)
 {
-	return 0*SD_SHARE_POWERLINE;
+	return 1*SD_SHARE_POWERLINE;
 }
 
 const struct cpumask *cpu_coregroup_mask(int cpu)
