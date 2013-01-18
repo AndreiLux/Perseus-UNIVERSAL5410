f64559b89bfa8501b7554988729f602e08c8aa28
Dan Magenheimer <dan.magenheimer@oracle.com>
2013-01-18 13:24:24 -0800

    staging: zcache: rename ramster to zcache
    [V2: no code changes, patchset now generated via git format-patch -M]
    
    In staging, rename ramster to zcache
    
    The original zcache in staging was a "demo" version, and this new zcache
    is a significant rewrite.  While certain disagreements were being resolved,
    both "old zcache" and "new zcache" needed to reside in the staging tree
    simultaneously.  In order to minimize code change and churn, the newer
    version of zcache was temporarily merged into the "ramster" staging driver
    which, prior to that, had at one time heavily leveraged the older version
    of zcache.  So, recently, "new zcache" resided in the ramster directory.
    
    Got that? No? Sorry, temporary political compromises are rarely pretty.
    
    The older version of zcache is no longer being maintained and has now
    been removed from the staging tree.  So now the newer version of zcache
    can rightfully reclaim sole possession of the name "zcache".
    
    FYI, this [PATCH 2/5] is simply a "git mv" generated by "git format-patch -M".
    
    Signed-off-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    
    	drivers/staging/ramster/cluster/heartbeat.h
    	drivers/staging/ramster/cluster/masklog.c
    	drivers/staging/ramster/cluster/masklog.h
    	drivers/staging/ramster/cluster/nodemanager.h
    	drivers/staging/ramster/cluster/ramster_nodemanager.h
    	drivers/staging/ramster/cluster/tcp.h
    	drivers/staging/ramster/cluster/tcp_internal.h
    	drivers/staging/ramster/ramster/heartbeat.h
    	drivers/staging/ramster/ramster/masklog.c
    	drivers/staging/ramster/ramster/masklog.h
    	drivers/staging/ramster/ramster/nodemanager.h
    	drivers/staging/ramster/ramster/ramster_nodemanager.h
    	drivers/staging/ramster/ramster/tcp.h
    	drivers/staging/ramster/ramster/tcp_internal.h
    	drivers/staging/zcache/ramster/heartbeat.c
    	drivers/staging/zcache/ramster/heartbeat.h
    	drivers/staging/zcache/ramster/masklog.c
    	drivers/staging/zcache/ramster/masklog.h
    	drivers/staging/zcache/ramster/nodemanager.c
    	drivers/staging/zcache/ramster/nodemanager.h
    	drivers/staging/zcache/ramster/r2net.c
    	drivers/staging/zcache/ramster/ramster.c
    	drivers/staging/zcache/ramster/ramster.h
    	drivers/staging/zcache/ramster/ramster_nodemanager.h
    	drivers/staging/zcache/ramster/tcp.c
    	drivers/staging/zcache/ramster/tcp.h
    	drivers/staging/zcache/ramster/tcp_internal.h
    	drivers/staging/zcache/zbud.c
    	drivers/staging/zcache/zbud.h
    
    Signed-off-by: Andrei F <luxneb@gmail.com>

--- drivers/staging/zcache/ramster/ramster.h
+++ drivers/staging/zcache/ramster/ramster.h
@@ -6,16 +6,47 @@
  * Copyright (c) 2009-2012, Dan Magenheimer, Oracle Corp.
  */
 
-#ifndef _RAMSTER_H_
-#define _RAMSTER_H_
+#ifndef _RAMSTER_RAMSTER_H_
+#define _RAMSTER_RAMSTER_H_
+
+#include "../tmem.h"
+
+enum ramster_remotify_op {
+	RAMSTER_REMOTIFY_FLUSH_PAGE,
+	RAMSTER_REMOTIFY_FLUSH_OBJ,
+};
+
+struct ramster_remotify_hdr {
+	enum ramster_remotify_op op;
+	struct list_head list;
+};
+
+struct flushlist_node {
+	struct ramster_remotify_hdr rem_op;
+	struct tmem_xhandle xh;
+};
+
+struct ramster_preload {
+	struct flushlist_node *flnode;
+};
+
+union remotify_list_node {
+	struct ramster_remotify_hdr rem_op;
+	struct {
+		struct ramster_remotify_hdr rem_op;
+		struct tmem_handle th;
+	} zbud_hdr;
+	struct flushlist_node flist;
+};
 
 /*
  * format of remote pampd:
- *   bit 0 == intransit
- *   bit 1 == is_remote... if this bit is set, then
- *   bit 2-9 == remotenode
- *   bit 10-22 == size
- *   bit 23-30 == cksum
+ *   bit 0 is reserved for zbud (in-page buddy selection)
+ *   bit 1 == intransit
+ *   bit 2 == is_remote... if this bit is set, then
+ *   bit 3-10 == remotenode
+ *   bit 11-23 == size
+ *   bit 24-31 == cksum
  */
 #define FAKE_PAMPD_INTRANSIT_BITS	1
 #define FAKE_PAMPD_ISREMOTE_BITS	1
@@ -23,7 +54,7 @@
 #define FAKE_PAMPD_REMOTESIZE_BITS	13
 #define FAKE_PAMPD_CHECKSUM_BITS	8
 
-#define FAKE_PAMPD_INTRANSIT_SHIFT	0
+#define FAKE_PAMPD_INTRANSIT_SHIFT	1
 #define FAKE_PAMPD_ISREMOTE_SHIFT	(FAKE_PAMPD_INTRANSIT_SHIFT + \
 					 FAKE_PAMPD_INTRANSIT_BITS)
 #define FAKE_PAMPD_REMOTENODE_SHIFT	(FAKE_PAMPD_ISREMOTE_SHIFT + \
@@ -106,13 +137,25 @@ static inline void *pampd_mask_intransit_and_remote(void *marked_pampd)
 	return (void *)pampd;
 }
 
-extern int ramster_remote_async_get(struct tmem_xhandle *,
+extern int r2net_remote_async_get(struct tmem_xhandle *,
 				bool, int, size_t, uint8_t, void *extra);
-extern int ramster_remote_put(struct tmem_xhandle *, char *, size_t,
+extern int r2net_remote_put(struct tmem_xhandle *, char *, size_t,
 				bool, int *);
-extern int ramster_remote_flush(struct tmem_xhandle *, int);
-extern int ramster_remote_flush_object(struct tmem_xhandle *, int);
+extern int r2net_remote_flush(struct tmem_xhandle *, int);
+extern int r2net_remote_flush_object(struct tmem_xhandle *, int);
 extern int r2net_register_handlers(void);
 extern int r2net_remote_target_node_set(int);
 
-#endif /* _TMEM_H */
+extern int ramster_remotify_pageframe(bool);
+extern void ramster_init(bool, bool, bool);
+extern void ramster_register_pamops(struct tmem_pamops *);
+extern int ramster_localify(int, struct tmem_oid *oidp, uint32_t, char *,
+				unsigned int, void *);
+extern void *ramster_pampd_free(void *, struct tmem_pool *, struct tmem_oid *,
+				uint32_t, bool);
+extern void ramster_count_foreign_pages(bool, int);
+extern int ramster_do_preload_flnode(struct tmem_pool *);
+extern void ramster_cpu_up(int);
+extern void ramster_cpu_down(int);
+
+#endif /* _RAMSTER_RAMSTER_H */
