d4c0c6292b7efa1f5377e724f0eae4ca49b2d090
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-03-16 17:55:37 +0100

    mm: Added filesystem dynamic read-ahead (Credits to Chad Gooldman)

--- block/blk-core.c
+++ block/blk-core.c
@@ -501,8 +501,7 @@ struct request_queue *blk_alloc_queue_node(gfp_t gfp_mask, int node_id)
 	if (q->id < 0)
 		goto fail_q;
 
-	q->backing_dev_info.ra_pages =
-			(VM_MAX_READAHEAD * 1024) / PAGE_CACHE_SIZE;
+	q->backing_dev_info.ra_pages = max_readahead_pages;
 	q->backing_dev_info.state = 0;
 	q->backing_dev_info.capabilities = BDI_CAP_MAP_COPY;
 	q->backing_dev_info.name = "block";
