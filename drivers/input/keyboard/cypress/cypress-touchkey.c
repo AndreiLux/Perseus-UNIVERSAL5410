851ef3efc5fda84dfa64c5ec69d8fa53d5e7c747
sorg <pour.garbage@gmail.com>
2013-08-17 16:26:14 +0400

    input: cypress: Add negative screen toggle
    Signed-off-by: Andrei F <luxneb@gmail.com>

--- drivers/input/keyboard/cypress/cypress-touchkey.c
+++ drivers/input/keyboard/cypress/cypress-touchkey.c
@@ -848,9 +848,27 @@ static int touchkey_firmware_update(struct touchkey_i2c *tkey_i2c)
 #endif
 
 #ifndef TEST_JIG_MODE
+
+static int mdnie_shortcut_enabled = 0;
+module_param_named(mdnie_shortcut_enabled, mdnie_shortcut_enabled, int, S_IRUGO | S_IWUSR | S_IWGRP);
+
+static inline int64_t get_time_inms(void) {
+	int64_t tinms;
+	struct timespec cur_time = current_kernel_time();
+	tinms =  cur_time.tv_sec * MSEC_PER_SEC;
+	tinms += cur_time.tv_nsec / NSEC_PER_MSEC;
+	return tinms;
+}
+
+extern void mdnie_toggle_negative(void);
+static int key_trg_cnt = 4;
+static int key_trg_ms = 300;
+
 static irqreturn_t touchkey_interrupt(int irq, void *dev_id)
 {
 	struct touchkey_i2c *tkey_i2c = dev_id;
+	static int64_t trigger_lasttime = 0;
+	static int trigger_count = 0;
 	u8 data[3];
 	int ret;
 	int retry = 10;
@@ -877,6 +895,21 @@ static irqreturn_t touchkey_interrupt(int irq, void *dev_id)
 	if (keycode_type <= 0 || keycode_type >= touchkey_count) {
 		dev_dbg(&tkey_i2c->client->dev, "keycode_type err\n");
 		return IRQ_HANDLED;
+	}
+
+	if ((touchkey_keycode[keycode_type] == KEY_MENU) && 
+		pressed && mdnie_shortcut_enabled)
+	{
+		if ((get_time_inms() - trigger_lasttime) < key_trg_ms) {
+			if (++trigger_count >= key_trg_cnt) {
+				mdnie_toggle_negative();
+				trigger_count = 0;
+			}
+		} else {
+				trigger_count = 0;
+		}
+		
+		trigger_lasttime = get_time_inms();
 	}
 
 	input_report_key(tkey_i2c->input_dev,
