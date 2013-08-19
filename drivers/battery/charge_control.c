aa5f146688a9c5d00a6e6d1e1edf16efadcf3f3a
Andrei F <luxneb@gmail.com>
2013-08-20 03:04:17 +0400

    battery: Add MAX77803/UNIVERSAL5410 charger control

---
+++ drivers/battery/charge_control.c
@@ -0,0 +1,140 @@
+/*
+ * charge_control.c - Battery charge control for MAX77803 on UNIVERSAL5410
+ *
+ * @Author	: Andrei F. <https://github.com/AndreiLux>
+ * @Date	: August 2013
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/battery/sec_battery.h>
+
+static struct sec_battery_info *info;
+
+enum charge_control_type {
+	INPUT = 0, CHARGE, OTHER
+};
+
+struct charge_control {
+	const struct device_attribute	attribute;
+	enum charge_control_type	type;
+	unsigned int			index;
+};
+
+static ssize_t show_charge_property(struct device *dev,
+				    struct device_attribute *attr, char *buf);
+
+static ssize_t store_charge_property(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t count);
+
+#define _charge(name_, type_, index_)	\
+{ 							\
+	.attribute = {					\
+			.attr = {			\
+				  .name = name_,	\
+				  .mode = 0664,		\
+				},			\
+			.show = show_charge_property,	\
+			.store = store_charge_property,	\
+		     },					\
+	.type 	= type_ ,				\
+	.index 	= index_ ,				\
+}
+
+struct charge_control charge_controls[] = {
+	_charge("ac_input_curr"	, INPUT	, POWER_SUPPLY_TYPE_MAINS),
+	_charge("ac_chrg_curr"	, CHARGE, POWER_SUPPLY_TYPE_MAINS),
+
+	_charge("sdp_input_curr", INPUT	, POWER_SUPPLY_TYPE_USB),
+	_charge("sdp_chrg_curr"	, CHARGE, POWER_SUPPLY_TYPE_USB),
+
+	_charge("dcp_input_curr", INPUT	, POWER_SUPPLY_TYPE_USB_DCP),
+	_charge("dcp_chrg_curr"	, CHARGE, POWER_SUPPLY_TYPE_USB_DCP),
+
+	_charge("cdp_input_curr", INPUT	, POWER_SUPPLY_TYPE_USB_CDP),
+	_charge("cdp_chrg_curr"	, CHARGE, POWER_SUPPLY_TYPE_USB_CDP),
+
+	_charge("aca_input_curr", INPUT	, POWER_SUPPLY_TYPE_USB_ACA),
+	_charge("aca_chrg_curr"	, CHARGE, POWER_SUPPLY_TYPE_USB_ACA),
+
+	_charge("misc_input_curr", INPUT, POWER_SUPPLY_TYPE_MISC),
+	_charge("misc_chrg_curr", CHARGE, POWER_SUPPLY_TYPE_MISC),
+
+	_charge("wpc_input_curr", INPUT	, POWER_SUPPLY_TYPE_WPC),
+	_charge("wpc_chrg_curr"	, CHARGE, POWER_SUPPLY_TYPE_WPC),
+};
+
+#define curr(index) info->pdata->charging_current[index]
+
+static ssize_t show_charge_property(struct device *dev,
+				    struct device_attribute *attr, char *buf)
+{
+	struct charge_control *control = (struct charge_control*)(attr);
+	int val = 0;
+
+	switch (control->type) {
+		case INPUT:	
+			val = curr(control->index).input_current_limit;
+			break;
+		case CHARGE:	
+			val = curr(control->index).fast_charging_current;
+			break;
+		default:
+			break;
+	}
+
+	return sprintf(buf, "%d", val);
+}
+
+static ssize_t store_charge_property(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t count)
+{
+	struct charge_control *control = (struct charge_control*)(attr);
+	unsigned int val;
+
+	if (sscanf(buf, "%u", &val) != 1)
+		return -EINVAL;
+
+	switch (control->type) {
+		case INPUT:
+			curr(control->index).input_current_limit = val;
+			break;
+		case CHARGE:
+			curr(control->index).fast_charging_current = val;
+			break;
+		default:
+			break;
+	}
+
+	return count;
+}
+
+extern int SIOP_INPUT_LIMIT_CURRENT;
+extern int SIOP_CHARGING_LIMIT_CURRENT;
+
+DEVICE_INT_ATTR(siop_input_limit, 0644, SIOP_INPUT_LIMIT_CURRENT);
+DEVICE_INT_ATTR(siop_charge_limit, 0644, SIOP_CHARGING_LIMIT_CURRENT);
+
+void charger_control_init(struct sec_battery_info *sec_info)
+{
+	int i;
+
+	info = sec_info;
+
+	for (i = 0; i < ARRAY_SIZE(charge_controls); i++)
+		if (device_create_file(info->dev, &charge_controls[i].attribute))
+			;;
+
+	i = device_create_file(info->dev, &dev_attr_siop_input_limit.attr);
+	i = device_create_file(info->dev, &dev_attr_siop_charge_limit.attr);
+}
