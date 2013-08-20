6134740308bd5ff3dca5abb4d39cb016865953b5
Alberto96 <albertoguanti@gmail.com>
2013-08-20 22:19:10 +0200

    Revert "dynamic fsync: don't disable fdatasync()"
    This reverts commit 2dde8c8cabc5d4f12fe58f947a8a8d08d51a3cd4.

--- fs/sync.c
+++ fs/sync.c
@@ -223,7 +223,7 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-#if 0
+#ifdef CONFIG_DYNAMIC_FSYNC
 	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
