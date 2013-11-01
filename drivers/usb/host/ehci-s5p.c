d0320ab7b4867fa3a63382829e741554b9a6665a
Samsung OSRC <samsung.osrc@gmail.com>
2013-11-02 01:42:23 +0400

    usb: host: ehci-s5p: Enter suspend only in auto runtime mode
    Signed-off-by: Andrei F <luxneb@gmail.com>

--- drivers/usb/host/ehci-s5p.c
+++ drivers/usb/host/ehci-s5p.c
@@ -283,7 +283,8 @@ int s5p_ehci_bus_suspend(struct usb_hcd *hcd)
 	ret = ehci_bus_suspend(hcd);
 
 	/* Decrease pm_count that was increased at s5p_ehci_resume func. */
-	pm_runtime_put_noidle(hcd->self.controller);
+	if (hcd->self.controller->power.runtime_auto)
+		pm_runtime_put_noidle(hcd->self.controller);
 
 	return ret;
 }
@@ -469,8 +470,7 @@ static ssize_t store_ehci_power(struct device *dev,
 		 * So, set the hub waiting 500ms autosuspend delay*/
 		if (hcd->self.root_hub)
 			pm_runtime_set_autosuspend_delay(
-				&hcd->self.root_hub->dev,
-				msecs_to_jiffies(500));
+				&hcd->self.root_hub->dev, 500);
 		/* mif allow the ehci runtime after enumeration */
 		pm_runtime_forbid(dev);
 #else
