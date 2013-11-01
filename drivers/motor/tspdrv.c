0be2dfa57c858fd0f2126746090dfee9b538efb7
Samsung OSRC <samsung.osrc@gmail.com>
2013-11-02 01:57:51 +0400

    drivers: motor: Return on false data sizes
    Signed-off-by: Andrei F <luxneb@gmail.com>

--- drivers/motor/tspdrv.c
+++ drivers/motor/tspdrv.c
@@ -394,6 +394,7 @@ static ssize_t write(struct file *file, const char *buf, size_t count,
 			** (Should never happen).
 			*/
 			DbgOut((KERN_EMERG "tspdrv: invalid buffer index.\n"));
+			return 0;
 		}
 
 		/* Check bit depth */
@@ -414,6 +415,7 @@ static ssize_t write(struct file *file, const char *buf, size_t count,
 			** (Should never happen).
 			*/
 			DbgOut((KERN_EMERG "tspdrv: invalid data size.\n"));
+			return 0;
 		}
 
 		/* Check actuator index */
